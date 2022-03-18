79a196f95ed546a43628a9f5314d378af51f321a
Martastain <martas@imm.cz>
2022-03-18 15:16:41 +0100

    feat: graphql. filter versions by latest/hero

--- openpype/graphql/resolvers/versions.py
+++ openpype/graphql/resolvers/versions.py
@@ -27,54 +27,104 @@ async def get_versions(
         list[str] | None, argdesc("List of version author user names to filter by.")
     ] = None,
     version: int = None,
+
+    latestOnly: Annotated[bool, argdesc("List only latest versions")] = False,
+    heroOnly: Annotated[bool, argdesc("List only hero versions")] = False,
+    heroOrLatestOnly: Annotated[bool, argdesc(
+        "List hero versions. If hero does not exist, list latest")] = False
+
 ) -> VersionsConnection:
     """Return a list of versions."""
 
     project_name = root.project_name
 
     #
-    # Conditions
+    # SQL
     #
 
-    conditions = []
+    sql_columns = [
+        "versions.id AS id",
+        "versions.version AS version",
+        "versions.subset_id AS subset_id",
+        "versions.task_id AS task_id",
+        "versions.thumbnail_id AS thumbnail_id",
+        "versions.author AS author",
+        "versions.attrib AS attrib",
+        "versions.data AS data",
+        "versions.active AS active",
+        "versions.created_at AS created_at",
+        "versions.updated_at AS updated_at",
+    ]
+
+    # sql_joins = []
+    sql_conditions = []
+
     if ids:
-        conditions.append(f"id IN {SQLTool.id_array(ids)}")
+        sql_conditions.append(f"id IN {SQLTool.id_array(ids)}")
     if version:
-        conditions.append(f"version = {version}")
+        sql_conditions.append(f"version = {version}")
     if authors:
-        conditions.append(f"author IN {SQLTool.id_array(authors)}")
+        sql_conditions.append(f"author IN {SQLTool.id_array(authors)}")
 
     if subset_ids:
-        conditions.append(f"subset_id IN {SQLTool.id_array(subset_ids)}")
+        sql_conditions.append(f"subset_id IN {SQLTool.id_array(subset_ids)}")
     elif root.__class__.__name__ == "SubsetNode":
-        conditions.append(f"subset_id = '{root.id}'")
+        sql_conditions.append(f"subset_id = '{root.id}'")
     if task_ids:
-        conditions.append(f"task_id IN {SQLTool.id_array(task_ids)}")
+        sql_conditions.append(f"task_id IN {SQLTool.id_array(task_ids)}")
     elif root.__class__.__name__ == "TaskNode":
-        conditions.append(f"task_id = '{root.id}'")
+        sql_conditions.append(f"task_id = '{root.id}'")
+
+    if latestOnly:
+        sql_conditions.append(
+            f"""
+            versions.id IN (
+            SELECT l.ids[array_upper(l.ids, 1)]
+            FROM project_{project_name}.version_list as l
+            )
+            """
+        )
+    elif heroOnly:
+        sql_conditions.append("versions.version < 0")
+    elif heroOrLatestOnly:
+        sql_conditions.append(
+            f"""
+            (versions.version < 0
+            OR versions.id IN (
+                SELECT l.ids[array_upper(l.ids, 1)]
+                FROM project_{project_name}.version_list as l
+                WHERE l.versions[1] >= 0
+            )
+            )
+            """
+        )
 
     #
     # Pagination
     #
 
     pagination = ""
+    order_by = "id"
     if first:
-        pagination += f"ORDER BY id ASC LIMIT {first}"
+        pagination += f"ORDER BY versions.{order_by} ASC LIMIT {first}"
         if after:
-            conditions.append(f"id > '{EntityID.parse(after)}'")
+            sql_conditions.append(f"versions.{order_by} > '{EntityID.parse(after)}'")
     elif last:
-        pagination += f"ORDER BY id DESC LIMIT {first}"
+        pagination += f"ORDER BY versions.{order_by} DESC LIMIT {first}"
         if before:
-            conditions.append(f"id < '{EntityID.parse(before)}'")
+            sql_conditions.append(f"versions.{order_by} < '{EntityID.parse(before)}'")
 
     #
     # Query
     #
 
     query = f"""
-        SELECT *
-        FROM project_{project_name}.versions
-        {SQLTool.conditions(conditions)}
+        SELECT
+            {", ".join(sql_columns)}
+        FROM
+            project_{project_name}.versions AS versions
+        {SQLTool.conditions(sql_conditions)}
+        {pagination}
     """
 
     return await resolve(
@@ -86,6 +136,7 @@ async def get_versions(
         first,
         last,
         context=info.context,
+        order_by=order_by
     )
 
 
