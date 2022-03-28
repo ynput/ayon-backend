6316a7f74b4943e7e00e3219c5b4940c50db1255
Martastain <martas@imm.cz>
2022-03-28 10:00:34 +0200

    chore: lib.postgres docstrings

--- openpype/lib/postgres.py
+++ openpype/lib/postgres.py
@@ -1,11 +1,11 @@
 import asyncpg
-from nxtools import logging
+import asyncpg.pool
 
 from openpype.config import pypeconfig
 
 
 class Postgres:
-    pool = None
+    pool: asyncpg.pool.Pool | None = None
     ForeignKeyViolationError = asyncpg.exceptions.ForeignKeyViolationError
     UniqueViolationError = asyncpg.exceptions.UniqueViolationError
 
@@ -16,27 +16,26 @@ class Postgres:
 
     @classmethod
     async def connect(cls):
-        logging.info("Connecting to PostgreSQL")
+        """Create a PostgreSQL connection pool."""
         cls.pool = await asyncpg.create_pool(pypeconfig.postgres_url)
-        # cls.acquire = cls.pool.acquire
 
     @classmethod
     async def execute(cls, query, *args):
+        """Execute a SQL query and return a status (e.g. 'INSERT 0 2')"""
         async with cls.pool.acquire() as connection:
             return await connection.execute(query, *args)
 
     @classmethod
     async def fetch(cls, query, *args):
+        """Run a query and return the results as a list of Record."""
         async with cls.pool.acquire() as connection:
             return await connection.fetch(query, *args)
 
     @classmethod
     async def iterate(cls, query, *args, transaction=None):
+        """Run a query and return a generator yielding resulting rows records."""
         if transaction:
-            # TODO: use the same mechanism as in the default
-            # (non-transactional) implementation
-            # TODO: This is probably not needed at all
-            # since iteration does not make much sense in transaction
+            statement = await transaction.prepare(query)
             async for record in transaction.fetch(query, *args):
                 yield record
             return
