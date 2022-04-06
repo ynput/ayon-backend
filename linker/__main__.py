2c5c0424381f2c114a3dfddd9278118e7e3e453a
Martastain <martas@imm.cz>
2022-04-06 11:15:20 +0200

    feat: smarter demo links prototype

--- linker/__main__.py
+++ linker/__main__.py
@@ -3,22 +3,74 @@ import asyncio
 from linker.linker import make_links
 from openpype.lib.postgres import Postgres
 
+# Each link type has input and output descriptions.
+# Resolver for both is the same and accepts the following:
+#
+#
+# folder_path: regex to match folder path
+# folder_type: type of folder (str)
+# folder_id: only used internally to resolve "same_folder" argument
+# subset_name: regex to match subset name
+# limit: count of random objects to resolve (only makes sense on output)
+#
+# Top level parameter 'same_folder' limit the output section result
+# to the same folder as the input.
+
 LINK_TYPES = [
-    ["breakdown", "subset", "folder"],
-    ["breakdown", "folder", "folder"],
-    ["generative", "version", "version"],
-    ["reference", "version", "version"],
+    "breakdown|subset|folder",
+    "breakdown|folder|folder",
+    "reference|version|version",
+]
+
+GEN_CONFIG = [
+    {
+        "link_type": "breakdown|subset|folder",
+        "input": {
+            "folder_type": "Asset",
+            "folder_path": "^assets/characters/.*",
+            "subset_name": "^model.*",
+        },
+        "output": {
+            "subset_name": "^rigMain$",
+            "limit": 20,
+        },
+        "same_folder": True,
+    },
+    {
+        "link_type": "breakdown|folder|folder",
+        "input": {
+            "folder_type": "Asset",
+            "folder_path": "^assets/characters/.*",
+        },
+        "output": {
+            "folder_type": "Shot",
+            "limit": 20,
+        },
+        "same_folder": False,
+    },
+    {
+        "link_type": "reference|version|version",
+        "input": {
+            "folder_type": "Asset",
+            "folder_path": "^assets/characters/.*",
+            "subset_name": "^rigMain$",
+        },
+        "output": {
+            "folder_type": "Shot",
+            "subset_name": "^workfileCompositing$",
+            "limit": 20,
+        },
+        "same_folder": False,
+    },
 ]
 
 
 async def create_link_type(
     project_name: str,
-    link_type: str,
-    input_type: str,
-    output_type: str,
+    link_type_name: str,
 ) -> None:
 
-    link_type_name = f"{link_type}|{input_type}|{output_type}"
+    link_type, input_type, output_type = link_type_name.split("|")
 
     await Postgres.execute(
         f"""
@@ -42,11 +94,17 @@ async def main():
 
     for project_name in project_names:
         await Postgres.execute(f"DELETE FROM project_{project_name}.link_types")
-        for link_type in LINK_TYPES:
-            await create_link_type(project_name, *link_type)
+        await Postgres.execute(f"DELETE FROM project_{project_name}.links")
+
+        if project_name.lower() != "demo_commercial":
+            # DEBUG ON A SMALL PROJECT
+            continue
 
         for link_type in LINK_TYPES:
-            await make_links(project_name, *link_type)
+            await create_link_type(project_name, link_type)
+
+        for link_type_config in GEN_CONFIG:
+            await make_links(project_name, link_type_config)
 
 
 if __name__ == "__main__":
--- linker/linker.py
+++ linker/linker.py
@@ -1,34 +1,30 @@
+from typing import Any, AsyncGenerator, Literal
+
 from nxtools import logging
 
-from openpype.entities import FolderEntity, SubsetEntity, VersionEntity
-from openpype.entities.core import ProjectLevelEntity
 from openpype.lib.postgres import Postgres
-from openpype.utils import EntityID
+from openpype.utils import EntityID, SQLTool
 
-
-def entity_from_record(entity_type, project_name, record):
-    return {"folder": FolderEntity, "subset": SubsetEntity, "version": VersionEntity}[
-        entity_type
-    ].from_record(project_name, record)
+DEBUG = False
 
 
 async def create_link(
-    input_entity: ProjectLevelEntity,
-    output_entity: ProjectLevelEntity,
-    link_type: str,
+    project_name: str,
+    input_id: str,
+    output_id: str,
+    link_type_name: str,
     **kwargs,
 ) -> None:
-    assert input_entity.project_name == output_entity.project_name
-    project_name = input_entity.project_name
 
+    link_type, input_type, output_type = link_type_name.split("|")
     link_id = EntityID.create()
-    link_type_name = (
-        f"{link_type}|{input_entity.entity_type}|{output_entity.entity_type}"
-    )
 
-    logging.info(
-        f"Creating {link_type} link between {input_entity} and {output_entity}"
-    )
+    if DEBUG:
+        logging.debug(
+            f"Creating {link_type} link between "
+            f" {input_type} {input_id} and "
+            f" {output_type} {output_id}"
+        )
     await Postgres.execute(
         f"""
         INSERT INTO project_{project_name}.links
@@ -37,29 +33,132 @@ async def create_link(
             ($1, $2, $3, $4, $5)
         """,
         link_id,
-        input_entity.id,
-        output_entity.id,
+        input_id,
+        output_id,
         link_type_name,
         kwargs,
     )
 
 
+async def query_entities(
+    project_name: str,
+    entity_type: str,
+    folder_path: str | None = None,
+    folder_type: str | None = None,
+    folder_id: str | None = None,
+    subset_name: str | None = None,
+    limit: int | None = None,
+    version: Literal["same", "latest"] | None = None,
+) -> AsyncGenerator[tuple[str, str], None]:
+    """Query entities from a project.
+
+    Returns a generator of tuples of (folder_id, entity_id) for a
+    given entity type and filters.
+    """
+
+    conditions = []
+
+    if folder_id is not None:
+        conditions.append(f"folder_id = '{folder_id}'")
+    if folder_type is not None:
+        conditions.append(f"folder_type = '{folder_type}'")
+    if folder_path is not None:
+        conditions.append(f"h.path ~* '{folder_path}'")
+    if subset_name is not None:
+        conditions.append(f"s.name ~* '{subset_name}'")
+
+    if limit is not None:
+        cols = ["f.id as folder_id", "s.id as subset_id", "v.id as version_id"]
+    else:
+        if entity_type == "folder":
+            cols = [
+                "distinct(f.id) as folder_id",
+                "s.id as subset_id",
+                "v.id as version_id",
+            ]
+        elif entity_type == "subset":
+            cols = [
+                "distinct(s.id) as subset_id",
+                "f.id as folder_id",
+                "v.id as version_id",
+            ]
+        elif entity_type == "version":
+            cols = [
+                "distinct(v.id) as version_id",
+                "f.id as folder_id",
+                "s.id as subset_id",
+            ]
+        else:
+            raise ValueError(f"Unknown entity type: {entity_type}")
+
+    cols.append("h.path as path")
+    cols.append("s.name as subset_name")
+    cols.append("s.family as subset_family")
+
+    query = f"""
+        SELECT {", ".join(cols)}
+        FROM
+            project_{project_name}.folders f
+        INNER JOIN
+            project_{project_name}.subsets s
+            ON f.id = s.folder_id
+        INNER JOIN
+            project_{project_name}.versions v
+            ON s.id = v.subset_id
+        LEFT JOIN
+            project_{project_name}.hierarchy h
+            ON f.id = h.id
+        {SQLTool.conditions(conditions)}
+        {'ORDER BY RANDOM()' if limit is not None else ''}
+    """
+    used: list[str] = []  # faster that trying to find out, how to distinct
+    async for row in Postgres.iterate(query):
+        if row[f"{entity_type}_id"] in used:
+            continue
+        used.append(row[f"{entity_type}_id"])
+        # print("returning", entity_type, row["path"], row["subset_name"])
+        yield row["folder_id"], row[f"{entity_type}_id"]
+        if limit is not None and len(used) >= limit:
+            break
+
+
 async def make_links(
     project_name: str,
-    link_type: str,
-    input_type: str,
-    output_type: str,
+    link_type_config: dict[str, Any],
 ) -> None:
     logging.info(f"Creating links in project {project_name}")
+    link_type_name = link_type_config["link_type"]
+    link_type, input_type, output_type = link_type_name.split("|")
 
-    in_query = f"SELECT * FROM project_{project_name}.{input_type}s"
-    out_query = f"""
-        SELECT * FROM project_{project_name}.{output_type}s
-        ORDER BY RANDOM() LIMIT 10
-    """
-    async for row in Postgres.iterate(in_query):
-        input_entity = entity_from_record(input_type, project_name, row)
+    if "input" not in link_type_config:
+        raise ValueError(f"Missing input config in link type {link_type_name}")
+    if "output" not in link_type_config:
+        raise ValueError(f"Missing output config in link type {link_type_name}")
 
-        async for outrow in Postgres.iterate(out_query):
-            output_entity = entity_from_record(output_type, project_name, outrow)
-            await create_link(input_entity, output_entity, link_type, author="martas")
+    count = 0
+    async for folder_id, input_id in query_entities(
+        project_name,
+        input_type,
+        **link_type_config["input"],
+    ):
+        lconfig = {}
+        if link_type_config.get("same_folder", False):
+            lconfig["folder_id"] = folder_id
+
+        async for _, output_id in query_entities(
+            project_name,
+            output_type,
+            **link_type_config["output"] | lconfig,
+        ):
+
+            await create_link(
+                project_name,
+                input_id,
+                output_id,
+                link_type_name,
+                author="martas",
+            )
+            count += 1
+    logging.goodnews(
+        f"Created {count} {link_type_name} links for project {project_name}"
+    )
