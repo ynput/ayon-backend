6972de5f26c5c9384b864ad03da2741190239a05
Martastain <martas@imm.cz>
2022-05-30 15:10:17 +0200

    refactor: settings overrides

--- api/settings/system.py
+++ api/settings/system.py
@@ -9,19 +9,12 @@ from openpype.settings.common import BaseSettingsModel
 from openpype.settings.system import SystemSettings, get_default_system_settings
 
 
-@router.get("/schema")
-async def get_system_settings_schema():
-    """Return a JSON schema for the system settings."""
-    return SystemSettings.schema()
+#
+# Temporary utils, before we store overrides in the database
+#
 
 
-@router.get("/schema/project")
-async def get_project_settings_schema():
-    """Return a JSON schema for the project settings."""
-    return Response(status_code=501)
-
-
-def get_studio_overrides():
+def _get_studio_overrides():
     try:
         with open("/tmp/studio.json") as f:
             return json.load(f)
@@ -29,16 +22,21 @@ def get_studio_overrides():
         return {}
 
 
-def set_studio_overrides(overrides: dict[str, Any]):
+def _set_studio_overrides(overrides: dict[str, Any]):
     with open("/tmp/studio.json", "w") as f:
         json.dump(overrides, f)
 
 
-def delete_studio_overrides():
+def _delete_studio_overrides():
     try:
         os.remove("/tmp/studio.json")
     except FileNotFoundError:
         pass
+
+
+#
+# Overrides
+#
 
 
 def apply_overrides(
@@ -67,42 +65,61 @@ def apply_overrides(
 def list_overrides(
     obj: BaseSettingsModel,
     override: dict[str, Any],
-    root: str = "root",
+    crumbs: list[str] = None,
 ) -> dict[str, Any]:
     result = {}
+
+    if crumbs is None:
+        crumbs = []
+        root = "root"
+    else:
+        root = "root_" + "_".join(crumbs)
 
     for name, field in obj.__fields__.items():
         child = getattr(obj, name)
         path = f"{root}_{name}"
+        chcrumbs = [*crumbs, name]
 
         if isinstance(child, BaseSettingsModel):
             if child._isGroup:
-                r = {"path": path, "type": "group"}
-                if name in override:
-                    r["level"] = "studio"
-                    r["value"] = override[name]
-                else:
-                    r["level"] = "default"
-                    r["value"] = child.dict()
-
-                result[path] = r
-            result.update(list_overrides(child, override.get(name, {}), path))
+                result[path] = {
+                    "path": chcrumbs,
+                    "type": "group",
+                    "level": "studio" if name in override else "default",
+                }
+            result.update(list_overrides(child, override.get(name, {}), chcrumbs))
 
         elif type(child) is list:
-            r = {"path": path, "type": "list"}
+            r = {"path": chcrumbs, "type": "list"}
             if name in override:
                 r["level"] = "studio"
-                r["value"] = override[name]
             else:
                 r["level"] = "default"
-                r["value"] = child
 
             result[path] = r
             for i, item in enumerate(child):
                 if isinstance(item, BaseSettingsModel):
-                    result.update(list_overrides(item, override.get(name, {}), f"{path}_{i}"))
+                    ovr = override.get(name, None)
+                    if ovr is None:
+                        ovr = {}
+                    else:
+                        ovr = ovr[i]
+                    result.update(list_overrides(item, ovr, [*chcrumbs, f"{i}"]))
+                    try:
+                        result.update(
+                            list_overrides(
+                                item, override.get(name, [])[i], [*chcrumbs, f"{i}"]
+                            )
+                        )
+                    except IndexError:
+                        pass
                 else:
-                    result[f"{path}_{i}"] = {"path": f"{path}_{i}", "level": "studio", "value": item,}
+                    result[f"{path}_{i}"] = {
+                        "path": [*chcrumbs, f"{i}"],
+                        "level": "default",
+                        "value": item,
+                    }
+                    # TODO: overrides!!!
 
         else:
             # Naive types
@@ -114,6 +131,7 @@ def list_overrides(
                 ovr_level = "default"
 
             result[path] = {
+                "path": chcrumbs,
                 "value": ovr_from,
                 "level": ovr_level,
             }
@@ -121,36 +139,70 @@ def list_overrides(
     return result
 
 
+def extract_overrides(
+    default: SystemSettings,
+    overriden: SystemSettings,
+    existing: dict[str, Any] = None,
+) -> dict[str, Any]:
+    result: dict[str, Any] = {}
+
+    def crawl(obj, ovr, ex, target):
+        for name, field in obj.__fields__.items():
+            child = getattr(obj, name)
+            if isinstance(child, BaseSettingsModel) and not child._isGroup:
+                if child.dict() != ovr.dict()[name] or (name in ex):
+                    target[name] = {}
+                    crawl(child, getattr(ovr, name), ex.get(name, {}), target[name])
+            else:
+                if getattr(ovr, name) != getattr(obj, name) or (name in ex):
+                    target[name] = ovr.dict()[name]
+
+    crawl(default, overriden, existing or {}, result)
+    return result
+
+
+#
+# Endpoints
+#
+
+
+@router.get("/system/schema")
+async def get_system_settings_schema():
+    """Return a JSON schema for the system settings."""
+    return SystemSettings.schema()
+
+
 @router.get("/system")
 async def get_system_settings() -> SystemSettings:
     """Return the system settings."""
     defaults = get_default_system_settings()
-    overrides = get_studio_overrides()
+    overrides = _get_studio_overrides()
     return apply_overrides(defaults, overrides)
 
 
-@router.patch("/system", response_class=Response)
-async def set_system_settings(overrides: dict[str, Any]):
+@router.put("/system", response_class=Response)
+async def set_system_settings(overrides: SystemSettings):
     """Set the system settings."""
-
-    # Validate the settings by passing them through the schema
     defaults = get_default_system_settings()
-    _ = apply_overrides(defaults, overrides)
-
-    set_studio_overrides(overrides)
+    old_overrides = _get_studio_overrides()
+    new_overrides = extract_overrides(defaults, overrides, old_overrides)
+    print(json.dumps(new_overrides, indent=2))
+    _set_studio_overrides(new_overrides)
     return Response(status_code=204)
 
 
 @router.delete("/system", response_class=Response)
-async def delete_system_settings():
-    """Delete the system settings."""
-    delete_studio_overrides()
+async def delete_studio_overrides():
+    """Delete the studio overrides for the system settings."""
+    _delete_studio_overrides()
     return Response(status_code=204)
 
 
 @router.get("/system/overrides")
-async def get_system_overrides():
-    """Return the system overrides."""
+async def get_studio_overrides():
+    """Return the studio overrides.
+    This is a helper endpoint for creating the settings form.
+    """
     defaults = get_default_system_settings()
-    overrides = get_studio_overrides()
+    overrides = _get_studio_overrides()
     return list_overrides(defaults, overrides)
