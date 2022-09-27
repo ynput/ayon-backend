954fbb790f21610620d5ffc82e6a9a59339bdea4
Martastain <martas@imm.cz>
2022-09-27 10:26:31 +0200

    removed deprecated /api/settings

--- api/settings/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ["router"]
-
-from settings import attributes
-from settings.router import router
-
-assert attributes
--- api/settings/router.py
+++
@@ -1,3 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/settings", tags=["Settings"])
