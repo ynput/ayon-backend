fb246b683266691b59012ca3527d7aad60760a84
Martastain <martas@imm.cz>
2022-11-21 11:44:13 +0100

    statuses in default anatomy presets

--- openpype/settings/anatomy/statuses.py
+++ openpype/settings/anatomy/statuses.py
@@ -1,13 +1,26 @@
+from typing import Literal
 from pydantic import Field
 
 from openpype.settings.common import BaseSettingsModel
 
 
+State = Literal["not_started", "in_progress", "done", "blocked"]
+
+
+def get_state_enum():
+    return [
+        {"value": "not_started", "label": "Not Started"},
+        {"value": "in_progress", "label": "In Progress"},
+        {"value": "done", "label": "Done"},
+        {"value": "blocked", "label": "Blocked"},
+    ]
+
+
 class Status(BaseSettingsModel):
     _layout: str = "compact"
     name: str = Field(..., title="Name", min_length=1, max_length=100)
-    icon: str = Field("view_kanban", title="Icon")
-    color: str = Field("#c0b0d0", title="Color")
+    color: str = Field("#cacaca", title="Color", widget="color")
+    state: State = Field("not_started", title="State", enum_resolver=get_state_enum)
     original_name: str | None = Field(None, scope="hidden")  # Used for renaming
 
     def __hash__(self):
@@ -15,5 +28,11 @@ class Status(BaseSettingsModel):
 
 
 default_statuses = [
-    Status(name="Unknown"),
+    Status(name="Not ready", color="#cacaca", state="not_started"),
+    Status(name="Ready to start", color="#cc99ff", state="not_started"),
+    Status(name="In progress", color="#3498db", state="in_progress"),
+    Status(name="Pending review", color="#f1c40f", state="in_progress"),
+    Status(name="Approved", color="#1cbc90", state="done"),
+    Status(name="On hold", color="#e74c3c", state="blocked"),
+    Status(name="Omitted", color="#0099ff", state="blocked"),
 ]
