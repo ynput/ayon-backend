008d8d3a4cf4f02a8deec0a32b8f91da58921b06
Martastain <martas@imm.cz>
2022-12-05 13:24:22 +0100

    feat: all production settings endpoint

---
+++ api/settings/settings.py
@@ -0,0 +1,56 @@
+from fastapi import Depends, APIRouter
+
+from openpype.addons import AddonLibrary
+from openpype.api import dep_current_user, ResponseFactory
+from openpype.entities import UserEntity
+from openpype.settings import BaseSettingsModel
+from openpype.types import OPModel
+
+
+router = APIRouter(
+    tags=["Addons"],
+    responses={
+        401: ResponseFactory.error(401),
+        403: ResponseFactory.error(403),
+    },
+)
+
+
+class GetProductionSettingsResponse(OPModel):
+    settings: dict[str, BaseSettingsModel]
+    versions: dict[str, str]
+
+
+@router.get("/settings/production")
+async def get_production_settings(
+     user: UserEntity = Depends(dep_current_user),
+):
+    """Return all addon settings for the project."""
+
+    library = AddonLibrary.getinstance()
+
+    active_versions = await library.get_active_versions()
+
+    result: dict[str, BaseSettingsModel] = {}
+    versions: dict[str, str] = {}
+
+    for addon_name, addon in library.items():
+        if addon_name not in active_versions:
+            continue
+        try:
+            production_version = active_versions[addon_name]["production"]
+        except KeyError:
+            continue
+
+        try:
+            active_addon = library.addon(addon_name, production_version)
+        except Exception:
+            continue
+
+        settings = await active_addon.get_studio_settings()
+        if settings is None:
+            continue
+        result[addon_name] = settings
+        versions[addon_name] = production_version
+
+    return GetProductionSettingsResponse(settings=result, versions=versions)
