998ab14126e00b8935b9129dd1afa0acb13e5d69
Martin Wacker <martas@imm.cz>
2023-05-19 14:21:59 +0200

    resolve endpoint (MVP)

--- api/resolve/__init__.py
+++ api/resolve/__init__.py
@@ -1,3 +1,4 @@
+import re
 from urllib.parse import parse_qs, urlparse
 
 from fastapi import APIRouter
@@ -9,34 +10,34 @@ router = APIRouter(tags=["URI resolver"])
 
 
 class ResolveRequestModel(OPModel):
-    uris: list[str] = Field(..., description="list of uris to resolve")
+    uris: list[str] = Field(..., title="URIs", description="list of uris to resolve")
 
 
 class ResolvedEntityModel(OPModel):
-    project_name: str = Field(..., description="Project name")
-    folder_id: str | None = Field(None, description="Folder id")
-    subset_id: str | None = Field(None, description="Subset id")
-    task_id: str | None = Field(None, description="Task id")
-    subset_id: str | None = Field(None, description="Subset id")
-    version_id: str | None = Field(None, description="Version id")
-    representation_id: str | None = Field(None, description="Representation id")
-    workfile_id: str | None = Field(None, description="Workfile id")
+    project_name: str = Field(..., title="Project name")
+    folder_id: str | None = Field(None, title="Folder id")
+    subset_id: str | None = Field(None, title="Subset id")
+    task_id: str | None = Field(None, title="Task id")
+    subset_id: str | None = Field(None, title="Subset id")
+    version_id: str | None = Field(None, title="Version id")
+    representation_id: str | None = Field(None, title="Representation id")
+    workfile_id: str | None = Field(None, title="Workfile id")
 
 
 class ResolvedURIModel(OPModel):
-    uri: str = Field(..., description="Resolved URI")
-    entities: list[ResolvedEntityModel] = Field(..., description="Resolved entities")
+    uri: str = Field(..., title="Resolved URI")
+    entities: list[ResolvedEntityModel] = Field(..., title="Resolved entities")
 
 
 class ParsedURIModel(OPModel):
-    uri: str = Field(..., description="Resolved URI")
-    project_name: str = Field(..., description="Project name")
-    path: str | None = Field(None, description="Path")
-    subset_name: str | None = Field(None, description="Subset name")
-    task_name: str | None = Field(None, description="Task name")
-    version_name: str | None = Field(None, description="Version name")
-    representation_name: str | None = Field(None, description="Representation name")
-    workfile_name: str | None = Field(None, description="Workfile name")
+    uri: str = Field(..., title="Resolved URI")
+    project_name: str = Field(..., title="Project name")
+    path: str | None = Field(None, title="Path")
+    subset_name: str | None = Field(None, title="Subset name")
+    task_name: str | None = Field(None, title="Task name")
+    version_name: str | None = Field(None, title="Version name")
+    representation_name: str | None = Field(None, title="Representation name")
+    workfile_name: str | None = Field(None, title="Workfile name")
 
 
 def parse_uri(uri: str) -> ParsedURIModel:
@@ -55,34 +56,35 @@ def parse_uri(uri: str) -> ParsedURIModel:
     ], f"Invalid scheme: {parsed_uri.scheme}"
 
     project_name = parsed_uri.netloc
-    assert NAME_REGEX.match(project_name), f"Invalid project name: {project_name}"
+    name_validator = re.compile(NAME_REGEX)
+    assert name_validator.match(project_name), f"Invalid project name: {project_name}"
 
-    path = parsed_uri.path or None
+    path = parsed_uri.path.strip("/") or None
 
     qs = parse_qs(parsed_uri.query)
 
     subset_name = qs.get("subset", [None])[0]
-    assert subset_name is None or NAME_REGEX.match(
+    assert subset_name is None or name_validator.match(
         subset_name
     ), f"Invalid subset name: {subset_name}"
 
     task_name = qs.get("task", [None])[0]
-    assert task_name is None or NAME_REGEX.match(
+    assert task_name is None or name_validator.match(
         task_name
     ), f"Invalid task name: {task_name}"
 
     version_name = qs.get("version", [None])[0]
-    assert version_name is None or NAME_REGEX.match(
+    assert version_name is None or name_validator.match(
         version_name
     ), f"Invalid version name: {version_name}"
 
     representation_name = qs.get("representation", [None])[0]
-    assert representation_name is None or NAME_REGEX.match(
+    assert representation_name is None or name_validator.match(
         representation_name
     ), f"Invalid representation name: {representation_name}"
 
     workfile_name = qs.get("workfile", [None])[0]
-    assert workfile_name is None or NAME_REGEX.match(
+    assert workfile_name is None or name_validator.match(
         workfile_name
     ), f"Invalid workfile name: {workfile_name}"
 
@@ -107,34 +109,65 @@ def parse_uri(uri: str) -> ParsedURIModel:
     )
 
 
-async def resolve_entities(req: ParsedURIModel) -> list[ResolvedEntityModel]:
-    return []
-    query = """
-        SELECT r.attrib->>'path'
-        FROM hierarchy as n
-        INNER JOIN subsets AS s ON n.id = s.folder_id
-        INNER JOIN versions AS v ON s.id = v.subset_id
-        INNER JOIN representations AS r ON v.id = r.version_id
+async def resolve_entities(conn, req: ParsedURIModel) -> list[ResolvedEntityModel]:
+    result = []
+    cols = ["h.id as folder_id"]
+    joins = []
+    conds = []
+
+    if req.task_name is not None or req.workfile_name is not None:
+        return []  # not implemented
+    else:
+        if req.representation_name is not None:
+            cols.extend(
+                ["s.id as subset_id", "v.id as version_id", "r.id as representation_id"]
+            )
+            joins.append("INNER JOIN subsets AS s ON h.id = s.folder_id")
+            joins.append("INNER JOIN versions AS v ON s.id = v.subset_id")
+            joins.append("INNER JOIN representations AS r ON v.id = r.version_id")
+            conds.append(f"r.name = '{req.representation_name}'")
+            if req.version_name is not None:
+                conds.append(f"v.version = {int(req.version_name.lstrip('v'))}")
+            if req.subset_name is not None:
+                conds.append(f"s.name = '{req.subset_name}'")
+            if req.path is not None:
+                conds.append(f"h.path = '{req.path}'")
+        elif req.version_name is not None:
+            cols.extend(["s.id as subset_id", "v.id as version_id"])
+            joins.append("INNER JOIN subsets AS s ON h.id = s.folder_id")
+            joins.append("INNER JOIN versions AS v ON s.id = v.subset_id")
+            conds.append(f"v.version = {int(req.version_name.lstrip('v'))}")
+            if req.subset_name is not None:
+                conds.append(f"s.name = '{req.subset_name}'")
+            if req.path is not None:
+                conds.append(f"h.path = '{req.path}'")
+        elif req.subset_name is not None:
+            cols.append("s.id as subset_id"),
+            joins.append("INNER JOIN subsets AS s ON h.id = s.folder_id")
+            conds.append(f"s.name = '{req.subset_name}'")
+            if req.path is not None:
+                conds.append(f"h.path = '{req.path}'")
+        elif req.path is not None:
+            conds.append(f"h.path = '{req.path}'")
+
+    query = f"""
+        SELECT
+            {", ".join(cols)}
+        FROM
+            hierarchy h
+            {" ".join(joins)}
         WHERE
-            n.path = $1
-        AND s.name = $2
-        AND v.version = $3
-        AND r.name = $4
+            {" AND ".join(conds)}
     """
 
-    for elm in args.split("&"):
-        key, val = elm.split("=")
-        if key == "subset":
-            subset = val
-        elif key == "version":
-            version = int(val.lower().lstrip("v"))
-        elif key == "representation":
-            representation = val
+    statement = await conn.prepare(query)
+    async for row in statement.cursor():
+        result.append(ResolvedEntityModel(project_name=req.project_name, **row))
 
-    result.append(await stmt.fetchval(hpath, subset, version, representation))
+    return result
 
 
-@router.post("/resolve")
+@router.post("/resolve", response_model_exclude_none=True)
 async def resolve(request: ResolveRequestModel) -> list[ResolvedURIModel]:
     result: list[ResolvedURIModel] = []
     current_project = ""
@@ -147,6 +180,6 @@ async def resolve(request: ResolveRequestModel) -> list[ResolvedURIModel]:
                         f"SET LOCAL search_path TO project_{parsed_uri.project_name}"
                     )
                     current_project = parsed_uri.project_name
-                entities = await resolve_entities(parsed_uri)
+                entities = await resolve_entities(conn, parsed_uri)
                 result.append(ResolvedURIModel(uri=uri, entities=entities))
     return result
