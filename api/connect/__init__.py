24e3651e23a0ad6ca4c637e51028498ce81d460c
Martin Wacker <martas@imm.cz>
2023-07-28 15:51:06 +0200

    YnputConnect proof of concept

---
+++ api/connect/__init__.py
@@ -0,0 +1,60 @@
+import httpx
+from fastapi import APIRouter
+
+from ayon_server.lib.postgres import Postgres
+from ayon_server.types import Field, OPModel
+
+router = APIRouter(
+    prefix="/connect",
+    tags=["YnputConnect"],
+)
+
+
+@router.get("")
+async def get_ynput_connect_info():
+    res = await Postgres.fetch(
+        """
+        SELECT value FROM secrets
+        WHERE name = 'ynput_connect_key'
+        """
+    )
+
+    if not res:
+        raise Exception("Ynput connect key not found")
+
+    key = res[0]["value"]
+
+    async with httpx.AsyncClient() as client:
+        res = await client.get(f"https://auth.ayon.cloud/info?key={key}")
+
+    return res.json()
+
+
+class YnputConnectRequestModel(OPModel):
+    key: str = Field(..., description="Ynput connect key")
+
+
+@router.post("")
+async def set_ynput_connect_key(request: YnputConnectRequestModel):
+    await Postgres.execute(
+        """
+        INSERT INTO secrets (name, value)
+        VALUES ('ynput_connect_key', $1)
+        ON CONFLICT (name) DO UPDATE SET value = $1
+        """,
+        request.key,
+    )
+
+    return {"success": True}
+
+
+@router.delete("")
+async def delete_ynput_connect_key():
+    await Postgres.execute(
+        """
+        DELETE FROM secrets
+        WHERE name = 'ynput_connect_key'
+        """
+    )
+
+    return {"success": True}
