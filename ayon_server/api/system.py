072a0f1c88239f42c325dc1f45091fd681aa8ff0
Martin Wacker <martas@imm.cz>
2023-08-02 22:09:32 +0200

    send sighup to gunicorn instead sigterm to reload server

--- ayon_server/api/system.py
+++ ayon_server/api/system.py
@@ -7,4 +7,9 @@ from nxtools import logging
 def restart_server():
     """Force the server to restart."""
     logging.warning("Server is restarting")
-    os.kill(os.getppid(), signal.SIGTERM)
+
+    # Send a SIGHUP to the parent process (gunicorn) to request a reload
+    # Gunicorn will restart the server when it receives this signal,
+    # but it won't quit itself, so the container will keep running.
+
+    os.kill(os.getppid(), signal.SIGHUP)
