b555603b1c5e56bc3b8f79ca781343953c06c245
Martastain <martas@imm.cz>
2023-08-03 16:11:10 +0200

    validate ynput connect key, allow setting it during firstRun and return user info

--- api/connect/__init__.py
+++ api/connect/__init__.py
@@ -1,7 +1,7 @@
 import httpx
 from fastapi import APIRouter
 
-from ayon_server.api.dependencies import CurrentUser
+from ayon_server.api.dependencies import CurrentUser, CurrentUserOptional
 from ayon_server.api.responses import EmptyResponse
 from ayon_server.exceptions import BadRequestException, ForbiddenException
 from ayon_server.lib.postgres import Postgres
@@ -11,6 +11,14 @@ router = APIRouter(
     prefix="/connect",
     tags=["YnputConnect"],
 )
+
+
+async def admin_exists() -> bool:
+    async for row in Postgres.iterate(
+        "SELECT name FROM users WHERE data->>'isAdmin' = 'true'"
+    ):
+        return True
+    return False
 
 
 @router.get("")
@@ -42,12 +50,28 @@ class YnputConnectRequestModel(OPModel):
     key: str = Field(..., description="Ynput connect key")
 
 
+class YnputConnectResponseModel(OPModel):
+    user_name: str = Field(..., description="User name")
+    user_email: str = Field(..., description="User email")
+
+
 @router.post("")
 async def set_ynput_connect_key(
-    request: YnputConnectRequestModel, user: CurrentUser
+    request: YnputConnectRequestModel, user: CurrentUserOptional
 ) -> EmptyResponse:
-    if not user.is_admin:
+    if user and not user.is_admin:
         raise ForbiddenException("Only admins can set the Ynput connect key")
+
+    if user is None:
+        has_admin = await admin_exists()
+        if has_admin:
+            raise ForbiddenException("Connecting to Ynput is allowed only on first run")
+
+    async with httpx.AsyncClient() as client:
+        res = await client.get(f"https://auth.ayon.cloud/info?key={request.key}")
+        if res.status_code != 200:
+            raise ForbiddenException("Invalid Ynput connect key")
+        data = res.json()
 
     await Postgres.execute(
         """
@@ -58,7 +82,10 @@ async def set_ynput_connect_key(
         request.key,
     )
 
-    return EmptyResponse()
+    return YnputConnectResponseModel(
+        user_name=data["name"],
+        user_email=data["email"],
+    )
 
 
 @router.delete("")
