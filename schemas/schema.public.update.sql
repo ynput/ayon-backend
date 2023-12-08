95e13388e12b1d39e57eda92e1d7140cb2bbf404
Martin Wacker <martas@imm.cz>
2023-12-08 14:47:11 +0100

    migrate siteId to instanceId

--- schemas/schema.public.update.sql
+++ schemas/schema.public.update.sql
@@ -156,3 +156,20 @@ CREATE OR REPLACE FUNCTION add_thumbnail_id_to_tasks ()
 
 SELECT add_thumbnail_id_to_tasks();
 DROP FUNCTION IF EXISTS add_thumbnail_id_to_tasks();
+
+--------------
+-- Ayon 0.8 --
+--------------
+
+-- Copy siteId to instanceId, if instanceId does not exist
+-- (this is a one-time migration)
+
+INSERT INTO config (key, value)
+SELECT 'instanceId', value
+FROM config
+WHERE key = 'siteId'
+AND NOT EXISTS (
+    SELECT 1
+    FROM config
+    WHERE key = 'instanceId'
+);
