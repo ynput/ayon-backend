16d24299b7e45a03a0785d49cbe7e150429ead61
Martastain <martas@imm.cz>
2023-12-13 00:27:24 +0100

    fix: removed references to ynput connect

--- api/connect/__init__.py
+++ api/connect/__init__.py
@@ -94,10 +94,8 @@ async def get_ynput_cloud_info(
 
 
 @router.get("/authorize")
-async def authorize_ynput_connect(
-    instance_id: InstanceID, origin_url: str = Query(...)
-):
-    """Redirect to Ynput connect authorization page"""
+async def connect_to_ynput_cloud(instance_id: InstanceID, origin_url: str = Query(...)):
+    """Redirect to Ynput cloud authorization page"""
 
     base_url = f"{ayonconfig.ynput_cloud_api_url}/api/v1/connect"
     params = f"instance_redirect={origin_url}&instance_id={instance_id}"
@@ -105,20 +103,22 @@ async def authorize_ynput_connect(
 
 
 @router.post("")
-async def set_ynput_connect_key(
+async def set_ynput_cloud_key(
     request: YnputConnectRequestModel,
     user: CurrentUserOptional,
     instance_id: InstanceID,
 ) -> YnputConnectResponseModel:
-    """Store the Ynput connect key in the database and return the user info"""
+    """Store the Ynput cloud key in the database and return the user info"""
 
     if user and not user.is_admin:
-        raise ForbiddenException("Only admins can set the Ynput connect key")
+        raise ForbiddenException("Only admins can set the Ynput Cloud key")
 
     if user is None:
         has_admin = await admin_exists()
         if has_admin:
-            raise ForbiddenException("Connecting to Ynput is allowed only on first run")
+            raise ForbiddenException(
+                "Connecting to Ynput Cloud is allowed only on first run"
+            )
 
     headers = {
         "x-ynput-cloud-instance": instance_id,
@@ -131,7 +131,7 @@ async def set_ynput_connect_key(
             headers=headers,
         )
         if res.status_code != 200:
-            raise ForbiddenException("Invalid Ynput connect key")
+            raise ForbiddenException("Invalid Ynput cloud key")
         data = res.json()
 
     await Postgres.execute(
@@ -147,10 +147,10 @@ async def set_ynput_connect_key(
 
 
 @router.delete("")
-async def delete_ynput_connect_key(user: CurrentUser) -> EmptyResponse:
-    """Remove the Ynput connect key from the database"""
+async def delete_ynput_cloud_key(user: CurrentUser) -> EmptyResponse:
+    """Remove the Ynput cloud key from the database"""
     if not user.is_admin:
-        raise ForbiddenException("Only admins can remove the Ynput connect key")
+        raise ForbiddenException("Only admins can remove the Ynput cloud key")
 
-    await Postgres.execute("DELETE FROM secrets WHERE name = 'ynput_connect_key'")
+    await Postgres.execute("DELETE FROM secrets WHERE name = 'ynput_cloud_key'")
     return EmptyResponse()
