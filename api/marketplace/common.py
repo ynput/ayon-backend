47331f9ef2d08e48e35a4efe317192895371dd5b
Martin Wacker <martas@imm.cz>
2023-12-20 10:49:47 +0100

    added local addon info

--- api/marketplace/common.py
+++ api/marketplace/common.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import httpx
 
+from ayon_server.addons.library import AddonLibrary
 from ayon_server.config import ayonconfig
 from ayon_server.exceptions import AyonException, ForbiddenException
 from ayon_server.lib.postgres import Postgres
@@ -60,3 +61,30 @@ async def get_marketplace_data(
     res.raise_for_status()  # should not happen
 
     return res.json()
+
+
+async def get_local_latest_addon_versions() -> dict[str, str]:
+    """Get the current latest versions of installed addons
+
+    Used to check if there are new versions available
+    """
+
+    result = {}
+    for addon_name, definition in AddonLibrary.items():
+        result[addon_name] = definition.latest.version
+    return result
+
+
+async def get_local_production_addon_versions() -> dict[str, str]:
+    """Get the current production versions of installed addons
+
+    Used to check if there are new versions available
+    """
+
+    res = await Postgres.fetch(
+        "SELECT data->'addons' as addons FROM bundles WHERE is_production"
+    )
+    if not res:
+        return {}
+
+    return res[0]["addons"] or {}
--- api/marketplace/marketplace.py
+++ api/marketplace/marketplace.py
@@ -1,20 +1,57 @@
-from .common import get_marketplace_data
+from .common import (
+    get_local_latest_addon_versions,
+    get_local_production_addon_versions,
+    get_marketplace_data,
+)
 from .router import router
 
 
 @router.get("/addons")
 async def market_addon_list():
     result = await get_marketplace_data("addons")
+    installed_addons = await get_local_latest_addon_versions()
+    production_addons = await get_local_production_addon_versions()
+
+    for addon in result.get("addons", []):
+        addon["currentProductionVersion"] = production_addons.get(addon["name"])
+        addon["currentLatestVersion"] = installed_addons.get(addon["name"])
     return result
 
 
 @router.get("/addons/{addon_name}")
 async def market_addon_detail(addon_name: str):
     result = await get_marketplace_data("addons", addon_name)
+    installed_addons = await get_local_latest_addon_versions()
+    production_addons = await get_local_production_addon_versions()
+    for version in result.get("versions", []):
+        version["isInstalled"] = (
+            addon_name in installed_addons
+            and installed_addons[addon_name] == version["version"]
+        )
+        version["isProduction"] = (
+            addon_name in production_addons
+            and production_addons[addon_name] == version["version"]
+        )
     return result
 
 
 @router.get("/addons/{addon_name}/{addon_version}")
 async def market_addon_version_detail(addon_name: str, addon_version: str):
     result = await get_marketplace_data("addons", addon_name, addon_version)
+
+    # is installed?
+
+    installed_addons = await get_local_latest_addon_versions()
+    result["isInstalled"] = (
+        addon_name in installed_addons and installed_addons[addon_name] == addon_version
+    )
+
+    # is production?
+
+    production_addons = await get_local_production_addon_versions()
+    result["isProduction"] = (
+        addon_name in production_addons
+        and production_addons[addon_name] == addon_version
+    )
+
     return result
