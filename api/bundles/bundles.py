2bcecd11860f4b2170a5043fcda9701099a145e7
Martin Wacker <martas@imm.cz>
2024-01-12 17:53:17 +0100

    fix: missing authorization for get bundles

--- api/bundles/bundles.py
+++ api/bundles/bundles.py
@@ -110,6 +110,7 @@ class ListBundleModel(OPModel):
 
 @router.get("/bundles", response_model_exclude_none=True)
 async def list_bundles(
+    user: CurrentUser,
     archived: bool = Query(False, description="Include archived bundles"),
 ) -> ListBundleModel:
     result: list[BundleModel] = []
