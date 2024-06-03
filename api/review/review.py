c60d59d57dc6db4e0794b206a82c9efcf6a54ad8
Martin Wacker <martas@imm.cz>
2024-06-03 11:03:23 +0200

    feat: add range request support for videos

--- api/review/review.py
+++ api/review/review.py
@@ -1,9 +1,10 @@
-from fastapi import Response
+from fastapi import Request
 
 from ayon_server.api.dependencies import CurrentUser, ProjectName, VersionID
 
 from .listing import ReviewableListModel
 from .router import router
+from .video import VideoResponse, serve_video
 
 
 @router.get("")
@@ -17,9 +18,10 @@ async def list_version_reviewables(
 
 @router.get("/{reviewable_id}")
 async def get_reviewable(
+    request: Request,
     user: CurrentUser,
     project_name: ProjectName,
     version_id: VersionID,
-    reviewable_id: int,
-):
-    return Response(content="Reviewable content")
+    reviewable_id: str,
+) -> VideoResponse:
+    return await serve_video(request, "/storage/server/pvw-placeholder.mp4")
---
+++ api/review/video.py
@@ -0,0 +1,88 @@
+import os
+
+import aiofiles
+from fastapi import Request, Response, status
+
+from ayon_server.exceptions import (
+    AyonException,
+    NotFoundException,
+    RangeNotSatisfiableException,
+)
+
+
+class VideoResponse(Response):
+    content_type = "video/mp4"
+
+
+async def get_bytes_range(file_name: str, start: int, end: int) -> bytes:
+    """Get a range of bytes from a file"""
+    async with aiofiles.open(file_name, mode="rb") as f:
+        await f.seek(start)
+        pos = start
+        read_size = end - pos + 1
+        return await f.read(read_size)
+
+
+def _get_range_header(range_header: str, file_size: int) -> tuple[int, int]:
+    try:
+        h = range_header.replace("bytes=", "").split("-")
+        start = int(h[0]) if h[0] != "" else 0
+        end = int(h[1]) if h[1] != "" else file_size - 1
+    except ValueError as e:
+        raise RangeNotSatisfiableException(str(e)) from e
+
+    if start > end or start < 0 or end > file_size - 1:
+        raise RangeNotSatisfiableException(f"Invalid range: {start}-{end}")
+    return start, end
+
+
+async def range_requests_response(
+    request: Request,
+    file_path: str,
+    content_type: str,
+) -> VideoResponse:
+    """Handle range requests for video files."""
+
+    file_size = os.stat(file_path).st_size
+    max_chunk_size = 1024 * 1024
+    range_header = request.headers.get("range")
+
+    headers = {
+        "content-type": content_type,
+        "accept-ranges": "bytes",
+        "content-encoding": "identity",
+        "content-length": str(file_size),
+        "access-control-expose-headers": (
+            "content-type, accept-ranges, content-length, "
+            "content-range, content-encoding"
+        ),
+    }
+    start = 0
+    end = file_size - 1
+    status_code = status.HTTP_200_OK
+
+    if range_header is not None:
+        start, end = _get_range_header(range_header, file_size)
+        end = min(end, start + max_chunk_size - 1)
+        size = end - start + 1
+        headers["content-length"] = str(size)
+        headers["content-range"] = f"bytes {start}-{end}/{file_size}"
+        status_code = status.HTTP_206_PARTIAL_CONTENT
+
+    payload = await get_bytes_range(file_path, start, end)
+
+    return VideoResponse(
+        content=payload,
+        headers=headers,
+        status_code=status_code,
+    )
+
+
+async def serve_video(request: Request, video_path: str) -> VideoResponse:
+    if not os.path.exists(video_path):
+        raise NotFoundException("Video not found")
+
+    try:
+        return await range_requests_response(request, video_path, "video/mp4")
+    except Exception as e:
+        raise AyonException(f"Error while serving video: {e}")
