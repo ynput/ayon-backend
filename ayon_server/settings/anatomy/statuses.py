3a851a4f0a54e17b43713cba775507511953d43c
Martastain <martas@imm.cz>
2024-07-19 17:10:42 +0200

    feat: entitytype scoped statuses

--- ayon_server/settings/anatomy/statuses.py
+++ ayon_server/settings/anatomy/statuses.py
@@ -1,8 +1,9 @@
-from typing import Literal
+from typing import Literal, get_args
 
 from pydantic import Field, validator
 
 from ayon_server.settings.common import BaseSettingsModel
+from ayon_server.types import ProjectLevelEntityType
 
 State = Literal["not_started", "in_progress", "done", "blocked"]
 
@@ -16,19 +17,40 @@ def get_state_enum():
     ]
 
 
+def scope_enum() -> list[dict[str, str]]:
+    return [
+        {"value": v, "label": v.capitalize()} for v in get_args(ProjectLevelEntityType)
+    ]
+
+
 class Status(BaseSettingsModel):
     _layout: str = "compact"
+    original_name: str | None = None  # Used for renaming, we don't show it in the UI
     name: str = Field(..., title="Name", min_length=1, max_length=100)
     shortName: str = Field("", title="Short name")
     state: State = Field("not_started", title="State", enum_resolver=get_state_enum)
     icon: str = Field("", title="Icon", widget="icon")
     color: str = Field("#cacaca", title="Color", widget="color")
-    original_name: str | None = Field(None, scope=[])  # Used for renaming
+    scope: list[str] | None = Field(
+        None,
+        example=["task"],
+        enum_resolver=scope_enum,
+        description=(
+            "Limit the status to specific entity types. "
+            " If empty, the status is available for all entity types."
+        ),
+    )
 
     @validator("original_name")
     def validate_original_name(cls, v, values):
         if v is None:
             return values["name"]
+        return v
+
+    @validator("scope")
+    def validate_scope(cls, v, values):
+        if v == []:
+            return None
         return v
 
     def __hash__(self):
@@ -42,6 +64,7 @@ default_statuses = [
         icon="fiber_new",
         color="#434a56",
         state="not_started",
+        scope=None,
     ),
     Status(
         name="Ready to start",
@@ -49,6 +72,7 @@ default_statuses = [
         icon="timer",
         color="#bababa",
         state="not_started",
+        scope=None,
     ),
     Status(
         name="In progress",
@@ -56,6 +80,7 @@ default_statuses = [
         icon="play_arrow",
         color="#3498db",
         state="in_progress",
+        scope=None,
     ),
     Status(
         name="Pending review",
@@ -63,6 +88,7 @@ default_statuses = [
         icon="visibility",
         color="#ff9b0a",
         state="in_progress",
+        scope=None,
     ),
     Status(
         name="Approved",
@@ -70,6 +96,7 @@ default_statuses = [
         icon="task_alt",
         color="#00f0b4",
         state="done",
+        scope=None,
     ),
     Status(
         name="On hold",
@@ -77,6 +104,7 @@ default_statuses = [
         icon="back_hand",
         color="#fa6e46",
         state="blocked",
+        scope=None,
     ),
     Status(
         name="Omitted",
@@ -84,5 +112,6 @@ default_statuses = [
         icon="block",
         color="#cb1a1a",
         state="blocked",
+        scope=None,
     ),
 ]
