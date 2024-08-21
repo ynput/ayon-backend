def0788b5461a2cdcadfd6a98ef57fb2fd20f7a7
Martastain <martas@imm.cz>
2024-08-21 16:20:37 +0200

    chore: added bundles migration... file

---
+++ api/bundles/migration.py
@@ -0,0 +1,8 @@
+async def migrate_settings_by_bundle(
+    source_bundle: str,
+    target_bundle: str,
+    source_variant: str,
+    target_variant: str,
+    with_projects: bool = True,
+) -> None:
+    pass  # TODO: do the magic here
