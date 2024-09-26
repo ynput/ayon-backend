ad8ea1a7cd951841ebc5465ede350f330463be0b
Martin Wacker <martas@imm.cz>
2024-09-26 11:27:20 +0200

    Update setup/attributes.py
    Co-authored-by: Luke Inderwick <49156310+Innders@users.noreply.github.com>

--- setup/attributes.py
+++ setup/attributes.py
@@ -27,9 +27,10 @@ DEFAULT_ATTRIBUTES: dict[str, dict[str, Any]] = {
         "title": "Priority",
         "default": "normal",
         "enum": [
-            {"value": "low", "label": "Low", "color": "#0000ff"},
-            {"value": "normal", "label": "Normal", "color": "#00ff00"},
-            {"value": "high", "label": "High", "color": "#ff0000"},
+            {"value": "low", "label": "Low", "color": "#9FA7B1", "icon": "keyboard_arrow_down"},
+            {"value": "normal", "label": "Normal", "color": "#9AC0E7", "icon": "check_indeterminate_small"},
+            {"value": "high", "label": "High", "color": "#FFAD66", "icon": "keyboard_arrow_up"},
+            {"value": "urgent", "label": "Urgent", "color": "#FF8585", "icon": "keyboard_double_arrow_up"},
         ],
     },
     "fps": {
