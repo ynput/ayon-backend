52f01886a491c059544df647adbad13ce8c1eebd
Martastain <martas@imm.cz>
2024-09-27 11:15:33 +0200

    feat: s3 generic byte upload methods

--- ayon_server/files/s3.py
+++ ayon_server/files/s3.py
@@ -2,7 +2,7 @@ import asyncio
 import os
 import time
 from concurrent.futures import ThreadPoolExecutor
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import boto3
 from fastapi import Request
@@ -63,7 +63,39 @@ async def get_signed_url(storage: "ProjectStorage", key: str, ttl: int = 3600) -
     return await run_in_threadpool(_get_signed_url, storage, key, ttl)
 
 
-# Upload to s3
+# Simple file store / retrieve
+
+
+def _store_s3_file(storage: "ProjectStorage", key: str, data: bytes) -> None:
+    client = _get_s3_client(storage)
+    client.put_object(Bucket=storage.bucket_name, Key=key, Body=data)
+
+
+async def store_s3_file(storage: "ProjectStorage", key: str, data: bytes) -> None:
+    await run_in_threadpool(_store_s3_file, storage, key, data)
+
+
+def _retrieve_s3_file(storage: "ProjectStorage", key: str) -> bytes:
+    client = _get_s3_client(storage)
+    response = client.get_object(Bucket=storage.bucket_name, Key=key)
+    return response["Body"].read()
+
+
+async def retrieve_s3_file(storage: "ProjectStorage", key: str) -> bytes:
+    return await run_in_threadpool(_retrieve_s3_file, storage, key)
+
+
+def _delete_s3_file(storage: "ProjectStorage", key: str):
+    client = _get_s3_client(storage)
+    client.delete_object(Bucket=storage.bucket_name, Key=key)
+
+
+async def delete_s3_file(storage: "ProjectStorage", key: str):
+    await run_in_threadpool(_delete_s3_file, storage, key)
+
+
+# Multipart upload to S3 with async queue
+# Used for larger files
 
 
 class S3Uploader:
@@ -75,8 +107,8 @@ class S3Uploader:
         self.bucket_name = bucket_name
 
         # Limited-size async queue for chunk uploads to prevent over-filling
-        self._queue = asyncio.Queue(maxsize=max_queue_size)
-        self._worker_task = None
+        self._queue: asyncio.Queue[bytes | None] = asyncio.Queue(maxsize=max_queue_size)
+        self._worker_task: asyncio.Task[Any] | None = None
         self._executor = ThreadPoolExecutor(max_workers=max_workers)
 
     def _init_file_upload(self, key: str):
@@ -135,7 +167,9 @@ class S3Uploader:
 
     async def init_file_upload(self, file_path: str):
         await asyncio.get_running_loop().run_in_executor(
-            self._executor, self._init_file_upload, file_path
+            self._executor,
+            self._init_file_upload,
+            file_path,
         )
         self._worker_task = asyncio.create_task(self._worker())
 
@@ -227,14 +261,3 @@ async def handle_s3_upload(
 
     logging.info(f"Uploaded {i} bytes to {path} in {upload_time:.2f} seconds")
     return i
-
-
-# S3 file management
-
-
-async def delete_s3_file(storage: "ProjectStorage", key: str):
-    bucket_name = storage.bucket_name
-    assert bucket_name
-    logging.debug(f"Deleting {key} from {bucket_name}", user="s3")
-    client = await get_s3_client(storage)
-    client.delete_object(Bucket=bucket_name, Key=key)
