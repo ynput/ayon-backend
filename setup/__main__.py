eebfe7035a1727b3ab4ae1b6dfbf941ed2784022
Martastain <martas@imm.cz>
2024-11-15 10:20:17 +0100

    refactor: move setup template to separate file

--- setup/__main__.py
+++ setup/__main__.py
@@ -1,43 +1,19 @@
 import asyncio
-import os
 import sys
-from base64 import b64decode
 from pathlib import Path
 from typing import Any
 
-import httpx
 from nxtools import critical_error, log_to_file, log_traceback, logging
 
 from ayon_server.config import ayonconfig
 from ayon_server.helpers.project_list import get_project_list
 from ayon_server.initialize import ayon_init
 from ayon_server.lib.postgres import Postgres
-from ayon_server.utils import json_loads
 from setup.access_groups import deploy_access_groups
 from setup.attributes import deploy_attributes
 from setup.initial_bundle import create_initial_bundle
+from setup.template import get_setup_template
 from setup.users import deploy_users
-
-# Defaults which should allow Ayon server to run out of the box
-
-DATA: dict[str, Any] = {
-    "addons": {},
-    "settings": {},
-    "users": [],
-    "roles": [],
-    "config": {},
-    "initialBundle": None,
-}
-
-if ayonconfig.force_create_admin:
-    DATA["users"] = [
-        {
-            "name": "admin",
-            "password": "admin",
-            "fullName": "Ayon admin",
-            "isAdmin": True,
-        },
-    ]
 
 
 async def db_migration(has_schema: bool) -> int:
@@ -45,39 +21,46 @@ async def db_migration(has_schema: bool) -> int:
 
     Returns the latest migration version applied
     (or available if db is not initialized).
+    That number is then saved in the database.
     """
 
-    if has_schema:
-        res = await Postgres.fetch("SELECT value FROM config WHERE key = 'dbVersion'")
-        if not res:
-            current_version = 0
-        else:
-            current_version = int(res[0]["value"])
-    else:
-        current_version = 0
-
-    logging.debug(f"Current database version: {current_version}")
+    # For future use:
+    # current_version = 0
+    # if has_schema:
+    #     res = await Postgres.fetch("SELECT value FROM config WHERE key = 'dbVersion'")
+    #     if res:
+    #         current_version = int(res[0]["value"])
 
     migrations_dir = "schemas/migrations"
     available_migrations = sorted(
         Path(migrations_dir).glob("*.sql"), key=lambda x: int(x.stem)
     )
 
+    logging.debug(f"Applying {len(available_migrations)} database migrations")
+    # logging.debug(f"Current database version: {current_version}")
+
+    # We evaluate has_schema here rather than in the main function,
+    # because we need to know the latest database version, even if
+    # the schema is not present yet.
     if has_schema:
         for migration in available_migrations:
             migration_version = int(migration.stem)
 
             # We still need to apply all migrations to ensure
             # imported project will have the same schema.
+
             # The following condition may be uncommented,
-            #
-            # When we switch project backup to json-based,
+            # when we switch project backup to json-based,
             # database aware format.
+
+            # Keep in mind that once this is enabled, we'll
+            # also be skipping the first (0) migration, that
+            # currently handles upgrades from < 1.0.0 era
 
             # if migration_version <= current_version:
             #    continue
+            # logging.info(f"Applying migration {migration_version}")
 
-            logging.info(f"Applying migration {migration_version}")
             await Postgres.execute(migration.read_text())
         return migration_version
 
@@ -127,70 +110,30 @@ async def main(force: bool | None = None) -> None:
     )
 
     # This is something we can do every time.
+    # Similar to database migrations, built-in attributes
+    # may change between versions, so we need to ensure
+    # they are up-to-date, when the container is started.
+
     await deploy_attributes()
 
-    TEMPLATE_ENV = "AYON_SETTINGS_TEMPLATE"
+    # When the setup is started for the first time, or
+    # is invoked using `make setup`, we  apply the
+    # setup template.
 
     if force_install:
-        logging.info("Force install requested")
-        template_data: dict[str, Any] = {}
-        if "-" in sys.argv:
-            logging.info("Reading setup file from stdin")
-            raw_data = sys.stdin.read()
-            try:
-                template_data = json_loads(raw_data)
-            except Exception:
-                log_traceback()
-                critical_error("Invalid setup file provided")
-
-        elif os.path.exists("/template.json"):
-            logging.info("Reading setup file from /template.json")
-            try:
-                raw_data = Path("/template.json").read_text()
-                template_data = json_loads(raw_data)
-            except Exception:
-                logging.warning("Invalid setup file provided. Using defaults")
-            else:
-                logging.debug("Setting up from /template.json")
-        elif raw_template_data := os.environ.get(TEMPLATE_ENV, ""):
-            logging.info(f"Reading setup file from {TEMPLATE_ENV} env variable")
-            try:
-                template_data = json_loads(b64decode(raw_template_data).decode())
-            except Exception:
-                logging.warning(
-                    f"Unable to parse {TEMPLATE_ENV} env variable. Using defaults"
-                )
-            else:
-                logging.debug(f"Setting up from {TEMPLATE_ENV} env variable")
-        else:
-            logging.warning("No setup file provided. Using defaults")
-        DATA.update(template_data)
-
-        if DATA.get("provisioningUrl"):
-            logging.info(f"Provisioning from {DATA['provisioningUrl']}")
-            headers = DATA.get("provisioningHeaders", {})
-            async with httpx.AsyncClient() as client:
-                try:
-                    response = await client.get(
-                        DATA["provisioningUrl"], headers=headers
-                    )
-                    response.raise_for_status()
-                except Exception:
-                    log_traceback()
-                else:
-                    DATA.update(response.json())
+        template = await get_setup_template()
 
         projects: list[str] = []
         async for row in Postgres.iterate("SELECT name FROM projects"):
             projects.append(row["name"])
 
-        users: list[dict[str, Any]] = DATA["users"]
-        access_groups: list[dict[str, Any]] = DATA.get("accessGroups", [])
+        users: list[dict[str, Any]] = template["users"]
+        access_groups: list[dict[str, Any]] = template.get("accessGroups", [])
 
         await deploy_users(users, projects)
         await deploy_access_groups(access_groups)
 
-        for name, value in DATA.get("secrets", {}).items():
+        for name, value in template.get("secrets", {}).items():
             await Postgres.execute(
                 """
                 INSERT INTO secrets (name, value)
@@ -201,7 +144,7 @@ async def main(force: bool | None = None) -> None:
                 value,
             )
 
-        for key, value in DATA.get("config", {}).items():
+        for key, value in template.get("config", {}).items():
             await Postgres.execute(
                 """
                 INSERT INTO config (key, value)
@@ -212,11 +155,14 @@ async def main(force: bool | None = None) -> None:
                 value,
             )
 
-    if bundle_data := DATA.get("initialBundle"):
-        if not isinstance(bundle_data, dict):
-            logging.warning("Invalid initial bundle data")
-        else:
-            await create_initial_bundle(bundle_data)
+        if bundle_data := template.get("initialBundle"):
+            if not isinstance(bundle_data, dict):
+                logging.warning("Invalid initial bundle data")
+            else:
+                await create_initial_bundle(bundle_data)
+
+    # Attributes may have changed, so we need to rebuild
+    # existing hierarchies.
 
     from ayon_server.helpers.inherited_attributes import rebuild_inherited_attributes
 
@@ -231,4 +177,9 @@ if __name__ == "__main__":
     logging.user = "setup"
     if ayonconfig.log_file is not None:
         logging.add_handler(log_to_file(ayonconfig.log_file))
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception:
+        log_traceback()
+        critical_error("Setup failed")
+    sys.exit(0)
