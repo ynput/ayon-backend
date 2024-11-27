8dd9a066f73fb0b8a61e492d6f4340e52bdaf24a
Martastain <martas@imm.cz>
2024-11-27 09:04:42 +0100

    chore: removed migration logging statement

--- schemas/migrations/00000004.sql
+++ schemas/migrations/00000004.sql
@@ -11,7 +11,6 @@ BEGIN
         AND table_schema = 'public'
         AND column_name = 'sender_type'
     ) THEN
-        RAISE WARNING 'Adding sender_type column to events';
         ALTER TABLE IF EXISTS public.events
         ADD COLUMN sender_type VARCHAR;
     END IF;
