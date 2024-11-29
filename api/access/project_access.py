13b817ed935df963e5dfb8c4390810da58a67441
Martastain <martas@imm.cz>
2024-11-29 14:09:18 +0100

    fix: remove user from non-existing project upon setting permissions

--- api/access/project_access.py
+++ api/access/project_access.py
@@ -8,6 +8,7 @@ from ayon_server.api.dependencies import CurrentUser
 from ayon_server.api.responses import EmptyResponse
 from ayon_server.auth.session import Session
 from ayon_server.entities.user import UserEntity
+from ayon_server.helpers.project_list import get_project_list
 from ayon_server.lib.postgres import Postgres
 
 from .router import router
@@ -51,6 +52,9 @@ async def set_projects_access(
 
     """
 
+    project_list = await get_project_list()
+    all_project_names = [project.name for project in project_list]
+
     # ensure the user has the necessary permissions
     projects_to_check = set()
     for user_name, project_access in payload.items():
@@ -73,6 +77,8 @@ async def set_projects_access(
             access_groups = copy.deepcopy(user.data.get("accessGroups", {}))
 
             for project_name, project_access_groups in project_access.items():
+                if project_name not in all_project_names:
+                    continue
                 if project_access_groups:
                     access_groups[project_name] = project_access_groups
                 else:
@@ -82,6 +88,9 @@ async def set_projects_access(
             for project_name, ags in access_groups.items():
                 if not ags:
                     ags_to_remove.add(project_name)
+                if project_name not in all_project_names:
+                    ags_to_remove.add(project_name)
+
             for project_name in ags_to_remove:
                 access_groups.pop(project_name, None)
 
