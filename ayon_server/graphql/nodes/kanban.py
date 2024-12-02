6690c1016bb5fa9e55ca2ec77513fc5532a18187
Martastain <martas@imm.cz>
2024-12-02 09:45:35 +0100

    fix: use correct thumbnail_info field in kanban

--- ayon_server/graphql/nodes/kanban.py
+++ ayon_server/graphql/nodes/kanban.py
@@ -59,7 +59,7 @@ def kanban_node_from_record(
     folder_priority = record.pop("folder_priority", None)
 
     thumbnail = None
-    thumb_data = record.pop("thumbnailInfo", None) or {}
+    thumb_data = record.pop("thumbnail_info", None) or {}
     if record["thumbnail_id"]:
         thumbnail = ThumbnailInfo(
             id=record["thumbnail_id"],
