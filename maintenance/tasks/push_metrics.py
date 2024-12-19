29097e6a3a6c469d0c0cc9d4648affe32d53a5bb
Martastain <martas@imm.cz>
2024-12-19 16:29:34 +0100

    feat: traffic statistics prototype

--- maintenance/tasks/push_metrics.py
+++ maintenance/tasks/push_metrics.py
@@ -1,5 +1,17 @@
+import datetime
+
+from ayon_server.lib.postgres import Postgres
 from ayon_server.metrics import post_metrics
 from maintenance.maintenance_task import StudioMaintenanceTask
+
+
+async def stats_cleanup():
+    now = datetime.datetime.now()
+    begin = now - datetime.timedelta(days=60)
+    begin_date = begin.strftime("%Y-%m-%d")
+
+    query = "DELETE FROM traffic_stats WHERE date < $1"
+    await Postgres().execute(query, begin_date)
 
 
 class PushMetrics(StudioMaintenanceTask):
@@ -7,3 +19,4 @@ class PushMetrics(StudioMaintenanceTask):
 
     async def main(self):
         await post_metrics()
+        await stats_cleanup()
