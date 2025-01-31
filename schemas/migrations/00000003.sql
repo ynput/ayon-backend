a934d1068c1d0f89da5dbc5154751aabee62ec95
Martastain <martas@imm.cz>
2025-01-31 15:31:39 +0100

    fix: remove temporary functions

--- schemas/migrations/00000003.sql
+++ schemas/migrations/00000003.sql
@@ -1,24 +1,18 @@
 ----------------
--- AYON 1.5 --
+-- AYON 1.5.0 --
 ----------------
 
 -- Add meta column to thumbnails
 -- Remove files.author foreign key
 
-CREATE OR REPLACE FUNCTION add_meta_column_to_thumbnails()
-   RETURNS VOID  AS
-   $$
-   DECLARE rec RECORD;
-   BEGIN
-        FOR rec IN select distinct nspname from pg_namespace where nspname like 'project_%'
-        LOOP
-            EXECUTE 'SET LOCAL search_path TO ' || quote_ident(rec.nspname);
-            ALTER TABLE IF EXISTS thumbnails ADD COLUMN IF NOT EXISTS meta JSONB DEFAULT '{}'::JSONB;
-            ALTER TABLE IF EXISTS files DROP CONSTRAINT IF EXISTS files_author_fkey;
-        END LOOP;
-        RETURN;
-   END;
-   $$ LANGUAGE plpgsql;
-
-SELECT add_meta_column_to_thumbnails();
-DROP FUNCTION IF EXISTS add_meta_column_to_thumbnails();
+DO $$
+DECLARE rec RECORD;
+BEGIN
+    FOR rec IN SELECT DISTINCT nspname FROM pg_namespace WHERE nspname LIKE 'project_%'
+    LOOP
+        EXECUTE 'SET LOCAL search_path TO ' || quote_ident(rec.nspname);
+        ALTER TABLE IF EXISTS thumbnails ADD COLUMN IF NOT EXISTS meta JSONB DEFAULT '{}'::JSONB;
+        ALTER TABLE IF EXISTS files DROP CONSTRAINT IF EXISTS files_author_fkey;
+    END LOOP;
+    RETURN;
+END $$;
