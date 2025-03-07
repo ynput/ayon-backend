d5f8244302e41083022138014814233dafa9d890
Martastain <martas@imm.cz>
2025-03-07 11:55:53 +0100

    chore: improved log messages

--- ayon_server/entities/core/attrib.py
+++ ayon_server/entities/core/attrib.py
@@ -39,10 +39,9 @@ class AttributeLibrary:
 
     def initial_load_thread(self) -> None:
         if Postgres.pool is not None:
-            logger.warning(
-                "Postgres pool exist durint attribute load. " "This should not happen.",
-                user="server",
-                handlers=None,
+            logger.error(
+                "Postgres pool exist during attribute load. " "This should not happen.",
+                nodb=True,
             )
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
