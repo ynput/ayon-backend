f1ddf3447a3f0556f2dd8c1c9632f511beadccb0
Martastain <martas@imm.cz>
2025-04-01 11:10:18 +0200

    fix: lock addon setting merging

--- api/settings/settings.py
+++ api/settings/settings.py
@@ -1,3 +1,4 @@
+import asyncio
 import traceback
 
 from fastapi import Query
@@ -14,6 +15,8 @@ from ayon_server.utils.request_coalescer import RequestCoalescer
 from .models import AddonSettingsItemModel, AllSettingsResponseModel
 from .router import router
 from .settings_addons_list import get_addon_list_for_settings
+
+lock = asyncio.Lock()
 
 
 @router.get("/settings", response_model_exclude_none=True)
@@ -105,147 +108,157 @@ async def get_all_settings(
     # Iterate over all addons and load the settings
     #
 
-    addon_result = []
-    for addon_name, addon_version in addon_list["addons"].items():
-        if addon_version is None:
-            continue
+    # we need to lock this part as pushing cache to addon
+    # could lead to race conditions. It is mostly cpu-bound anyways,
+    # so it shouldn't have a performance impact as the event loop
+    # is already blocked.
 
-        try:
-            addon = AddonLibrary.addon(addon_name, addon_version)
-        except NotFoundException:
-            logger.warning(
-                f"Addon {addon_name} {addon_version} "
-                f"declared in {bundle_name} not found"
-            )
+    async with lock:
+        addon_result = []
+        for addon_name, addon_version in addon_list["addons"].items():
+            if addon_version is None:
+                continue
 
-            broken_reason = AddonLibrary.is_broken(addon_name, addon_version)
-
-            addon_result.append(
-                AddonSettingsItemModel(
-                    name=addon_name,
-                    title=addon_name,
-                    version=addon_version,
-                    settings={},
-                    site_settings=None,
-                    is_broken=bool(broken_reason),
-                    reason=broken_reason,
-                )
-            )
-            continue
-
-        if project_bundle_name and addon_name not in addon_list["inherited_addons"]:
-            overridable = addon.get_project_can_override_addon_version()
-            if not overridable:
-                logger.error(
+            try:
+                addon = AddonLibrary.addon(addon_name, addon_version)
+            except NotFoundException:
+                logger.warning(
                     f"Addon {addon_name} {addon_version} "
-                    f"declared in project bundle {project_bundle_name} "
-                    f"cannot be overridden per project"
+                    f"declared in {bundle_name} not found"
+                )
+
+                broken_reason = AddonLibrary.is_broken(addon_name, addon_version)
+
+                addon_result.append(
+                    AddonSettingsItemModel(
+                        name=addon_name,
+                        title=addon_name,
+                        version=addon_version,
+                        settings={},
+                        site_settings=None,
+                        is_broken=bool(broken_reason),
+                        reason=broken_reason,
+                    )
                 )
                 continue
 
-        # Determine which scopes addon has settings for
-
-        model = addon.get_settings_model()
-        has_settings = False
-        has_project_settings = False
-        has_project_site_settings = False
-        has_site_settings = bool(addon.site_settings_model)
-        if model:
-            has_project_settings = False
-            for field_name, field in model.__fields__.items():
-                scope = field.field_info.extra.get("scope", ["studio", "project"])
-                if "project" in scope:
-                    has_project_settings = True
-                if "site" in scope:
-                    has_project_site_settings = True
-                if "studio" in scope:
-                    has_settings = True
-
-        # Load settings for the addon
-
-        site_settings = None
-        settings: BaseSettingsModel | None = None
-
-        try:
-            key = addon.name, addon_version
-            scache = all_settings.get(key, AddonSettingsCache())
-            addon.settings_cache = scache
-
-            if site_id:
-                site_settings = await addon.get_site_settings(user.name, site_id)
-
-                if project_name is None:
-                    # Studio level settings (studio level does not have)
-                    # site overrides per se but it can have site settings
-                    settings = await addon.get_studio_settings(variant)
-                else:
-                    # Project and site is requested, so we are returning
-                    # project level settings WITH site overrides
-                    settings = await addon.get_project_site_settings(
-                        project_name,
-                        user.name,
-                        site_id,
-                        variant,
+            if project_bundle_name and addon_name not in addon_list["inherited_addons"]:
+                overridable = addon.get_project_can_override_addon_version()
+                if not overridable:
+                    logger.error(
+                        f"Addon {addon_name} {addon_version} "
+                        f"declared in project bundle {project_bundle_name} "
+                        f"cannot be overridden per project"
                     )
-            elif project_name:
-                # Project level settings (no site overrides)
-                settings = await addon.get_project_settings(project_name, variant)
-            else:
-                # Just studio level settings (no project, no site)
-                settings = await addon.get_studio_settings(variant)
+                    continue
 
-        except Exception:
-            log_traceback(f"Unable to load {addon_name} {addon_version} settings")
+            # Determine which scopes addon has settings for
+
+            model = addon.get_settings_model()
+            has_settings = False
+            has_project_settings = False
+            has_project_site_settings = False
+            has_site_settings = bool(addon.site_settings_model)
+            if model:
+                has_project_settings = False
+                for field_name, field in model.__fields__.items():
+                    scope = field.field_info.extra.get("scope", ["studio", "project"])
+                    if "project" in scope:
+                        has_project_settings = True
+                    if "site" in scope:
+                        has_project_site_settings = True
+                    if "studio" in scope:
+                        has_settings = True
+
+            # Load settings for the addon
+
+            site_settings = None
+            settings: BaseSettingsModel | None = None
+
+            try:
+                key = addon.name, addon_version
+                scache = all_settings.get(key, AddonSettingsCache())
+                addon.settings_cache = scache
+
+                if site_id:
+                    site_settings = await addon.get_site_settings(user.name, site_id)
+
+                    if project_name is None:
+                        # Studio level settings (studio level does not have)
+                        # site overrides per se but it can have site settings
+                        settings = await addon.get_studio_settings(variant)
+                    else:
+                        # Project and site is requested, so we are returning
+                        # project level settings WITH site overrides
+                        settings = await addon.get_project_site_settings(
+                            project_name,
+                            user.name,
+                            site_id,
+                            variant,
+                        )
+                elif project_name:
+                    # Project level settings (no site overrides)
+                    settings = await addon.get_project_settings(project_name, variant)
+                else:
+                    # Just studio level settings (no project, no site)
+                    settings = await addon.get_studio_settings(variant)
+
+            except Exception:
+                log_traceback(f"Unable to load {addon_name} {addon_version} settings")
+                addon_result.append(
+                    AddonSettingsItemModel(
+                        name=addon_name,
+                        title=addon_name,
+                        version=addon_version,
+                        settings={},
+                        site_settings=None,
+                        is_broken=True,
+                        reason={
+                            "error": "Unable to load settings",
+                            "traceback": traceback.format_exc(),
+                        },
+                    )
+                )
+                continue
+
+            finally:
+                # clean-up pre-cached settings from the addon
+                addon.settings_cache = None
+
+            # Add addon to the result
+
             addon_result.append(
                 AddonSettingsItemModel(
                     name=addon_name,
-                    title=addon_name,
+                    title=addon.title if addon.title else addon_name,
                     version=addon_version,
-                    settings={},
-                    site_settings=None,
-                    is_broken=True,
-                    reason={
-                        "error": "Unable to load settings",
-                        "traceback": traceback.format_exc(),
-                    },
+                    # Has settings means that addon has settings model
+                    has_settings=has_settings,
+                    has_project_settings=has_project_settings,
+                    has_project_site_settings=has_project_site_settings,
+                    has_site_settings=has_site_settings,
+                    # Has overrides means that addon has overrides for the requested
+                    # project/site
+                    has_studio_overrides=settings._has_studio_overrides
+                    if settings
+                    else None,
+                    has_project_overrides=settings._has_project_overrides
+                    if settings
+                    else None,
+                    has_project_site_overrides=settings._has_site_overrides
+                    if settings
+                    else None,
+                    settings=settings.dict() if (settings and not summary) else {},
+                    site_settings=site_settings,
                 )
             )
-            continue
 
-        # Add addon to the result
+        addon_result.sort(key=lambda x: x.title.lower())
 
-        addon_result.append(
-            AddonSettingsItemModel(
-                name=addon_name,
-                title=addon.title if addon.title else addon_name,
-                version=addon_version,
-                # Has settings means that addon has settings model
-                has_settings=has_settings,
-                has_project_settings=has_project_settings,
-                has_project_site_settings=has_project_site_settings,
-                has_site_settings=has_site_settings,
-                # Has overrides means that addon has overrides for the requested
-                # project/site
-                has_studio_overrides=settings._has_studio_overrides
-                if settings
-                else None,
-                has_project_overrides=settings._has_project_overrides
-                if settings
-                else None,
-                has_project_site_overrides=settings._has_site_overrides
-                if settings
-                else None,
-                settings=settings.dict() if (settings and not summary) else {},
-                site_settings=site_settings,
-            )
+        return AllSettingsResponseModel(
+            bundle_name=addon_list["bundle_name"],
+            addons=addon_result,
+            inherited_addons=list(addon_list["inherited_addons"])
+            if project_bundle_name
+            else [],
         )
-
-    addon_result.sort(key=lambda x: x.title.lower())
-
-    return AllSettingsResponseModel(
-        bundle_name=addon_list["bundle_name"],
-        addons=addon_result,
-        inherited_addons=list(addon_list["inherited_addons"])
-        if project_bundle_name
-        else [],
-    )
