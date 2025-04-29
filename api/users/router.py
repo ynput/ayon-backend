d7c2d562d3c49fe472da40e05d65d1ef8d3666f2
Martastain <martas@imm.cz>
2025-04-29 14:37:04 +0200

    refactor: logging middleware

--- api/users/router.py
+++ api/users/router.py
@@ -1,9 +1,3 @@
 from fastapi import APIRouter
 
-from ayon_server.api.responses import ResponseFactory
-
-router = APIRouter(
-    prefix="/users",
-    tags=["Users"],
-    responses={401: ResponseFactory.error(401)},
-)
+router = APIRouter(prefix="/users", tags=["Users"])
