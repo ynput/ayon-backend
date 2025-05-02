cf293f6afc9b58dc22985c4de2b8de4ec553290d
Martastain <martas@imm.cz>
2025-05-02 10:24:55 +0200

    refactor: new action response model (w.i.p.)

--- ayon_server/actions/execute.py
+++ ayon_server/actions/execute.py
@@ -1,5 +1,5 @@
 import urllib.parse
-from typing import Any, Literal
+from typing import Annotated, Any, Literal, NotRequired, Required, TypedDict
 
 from ayon_server.actions.context import ActionContext
 from ayon_server.entities import UserEntity
@@ -8,33 +8,121 @@ from ayon_server.forms import SimpleForm
 from ayon_server.types import Field, OPModel
 from ayon_server.utils import create_hash
 
+ResponseType = Literal[
+    "simple",  # Just display a message
+    "launcher",  # Open the Ayon Launcher
+    "form",  # Display a form
+    "redirect",  # Redirect to a different URL or open a browser tab
+    "query",  # Modify the URL query parameters (web only)
+]
+
+#
+# Response payloads
+#
+
+
+class BaseResponsePayload(TypedDict):
+    """
+    BaseResponsePayload defines `extra_*` fields
+    that are used to pass sub-actions to the client
+    such as "copy to clipboard" or "download file"
+    sub-actions aren't mutually exclusive with the main action
+    that is defined by the response type field and can be
+    always used
+    """
+
+    extra_copy: NotRequired[str]
+    extra_download: NotRequired[str]
+
+
+class LauncherResponsePayload(BaseResponsePayload):
+    """
+    LauncherResponsePayload is used to open the Ayon Launcher
+    with the given URI.
+    """
+
+    uri: Required[str]
+
+
+class QueryResponsePayload(BaseResponsePayload):
+    """
+    QueryResponsePayload is used to modify the URL query parameters
+    of the current page. This only works in the web interface.
+    """
+
+    query: Required[dict[str, str | int | float | bool]]
+
+
+class RedirectResponsePayload(BaseResponsePayload):
+    """
+    RedirectResponsePayload is used to redirect the user
+    to a different URL or open a browser tab.
+    """
+
+    uri: Required[str]
+    new_tab: NotRequired[bool]
+
+
+class FormResponsePayload(BaseResponsePayload):
+    schema: Required[SimpleForm]
+    title: Required[str]
+    submit_label: NotRequired[str]
+    submit_icon: NotRequired[str]
+    cancel_label: NotRequired[str]
+    cancel_icon: NotRequired[str]
+
+
+#
+# Response model
+#
+
 
 class ExecuteResponseModel(OPModel):
-    type: Literal["launcher", "server"] = Field(
-        ...,
-        description="The type of response",
-        example="launcher",
-    )
-    success: bool = Field(
-        True,
-        description="Whether the action was successful",
-    )
-    message: str | None = Field(
-        None,
-        description="The message to display",
-        example="Action executed successfully",
-    )
+    type: Annotated[
+        ResponseType,
+        Field(
+            description="The type of response",
+            example="launcher",
+        ),
+    ] = "simple"
 
-    uri: str | None = Field(
-        None,
-        description="The uri to call from the browser",
-        example="ayon-launcher://action?server_url=http%3A%2F%2Flocalhost%3A8000%2F&token=eyJaaaa",
-    )
+    success: Annotated[
+        bool,
+        Field(
+            title="Action success",
+            description=(
+                "Payload is still parsed even if the action failed, "
+                "but the message is highlighted as an error."
+                "If the action execution is broken beyond repair, "
+                "Raise an exception instead of returning a response."
+            ),
+        ),
+    ] = True
 
-    payload: dict[str, Any] | None = Field(
-        None,
-        description="The payload of the request",
-    )
+    message: Annotated[
+        str | None,
+        Field(
+            description="The message to display",
+            example="Action executed successfully",
+        ),
+    ] = None
+
+    payload: Annotated[
+        dict[str, Any] | None,
+        Field(
+            title="Response payload",
+            description=(
+                "The payload of the response. "
+                "Payload model is parsed by the client and its schema, "
+                "is based on the type of action."
+            ),
+        ),
+    ] = None
+
+
+#
+# Action executor class
+#
 
 
 class ActionExecutor:
@@ -46,6 +134,26 @@ class ActionExecutor:
     variant: str
     identifier: str
     context: ActionContext
+
+    async def get_action_config(self) -> dict[str, Any]:
+        """Get action config for the given hash.
+
+        This is used to get the action config from the database
+        and return it to the user.
+        """
+        from ayon_server.addons.library import AddonLibrary
+
+        addon = AddonLibrary.addon(
+            self.addon_name,
+            self.addon_version,
+        )
+
+        return await addon.get_action_config(
+            identifier=self.identifier,
+            context=self.context,
+            user=self.user,
+            variant=self.variant,
+        )
 
     async def get_launcher_action_response(
         self,
@@ -95,8 +203,10 @@ class ActionExecutor:
         return ExecuteResponseModel(
             success=True,
             type="launcher",
-            uri=f"ayon-launcher://action?server_url={encoded_url}&token={hash}",
             message=message,
+            payload={
+                "uri": f"ayon-launcher://action?server_url={encoded_url}&token={hash}",
+            },
         )
 
     async def get_server_action_response(
@@ -113,49 +223,41 @@ class ActionExecutor:
     ) -> ExecuteResponseModel:
         """Return a response for a server actions
 
-        Server actions are actions that are only executed on the server.
-        They only return a message to display in the frontend
-        after the action is executed.
+        This is a deprecated method. Use specific methods for each action type instead.
+        This is provided for backward compatibility, will be removed in the future
+        and it does not support all action types or features.
         """
 
         if message is None:
             message = f"Action {self.identifier} executed successfully"
 
-        payload = {**kwargs}
+        payload: dict[str, Any] = {**kwargs}
+        response_type = "simple"
+
+        # Mutualy exclusive action types
+
         if query_params:
-            payload["__queryParams"] = query_params
-        if navigate:
-            payload["__navigate"] = navigate
-        if download:
-            payload["__download"] = download
+            response_type = "query"
+            payload["query"] = query_params
+        elif navigate:
+            response_type = "redirect"
+            payload["url"] = navigate
+        elif form:
+            response_type = "form"
+            payload["form_schema"] = list(form)
+            payload["form_title"] = message
+
+        # Sub-actions (additional behavior of the response)
+        # Such as "copy somethig to clipboard"
+
         if copy:
-            payload["__copy"] = copy
-        if form:
-            payload["__form"] = list(form)
+            payload["extra_copy"] = copy
+        if download:
+            payload["extra_download"] = download
 
         return ExecuteResponseModel(
             success=success,
-            type="server",
+            type=response_type,
             message=message,
             payload=payload,
-        )
-
-    async def get_action_config(self) -> dict[str, Any]:
-        """Get action config for the given hash.
-
-        This is used to get the action config from the database
-        and return it to the user.
-        """
-        from ayon_server.addons.library import AddonLibrary
-
-        addon = AddonLibrary.addon(
-            self.addon_name,
-            self.addon_version,
-        )
-
-        return await addon.get_action_config(
-            identifier=self.identifier,
-            context=self.context,
-            user=self.user,
-            variant=self.variant,
         )
