fc0b516d103faa8ae1b3ea59b0b15e5b73f3ff4c
Martastain <martas@imm.cz>
2025-06-16 10:54:29 +0200

    feat: filtering folders, products, versions and representations

--- ayon_server/graphql/resolvers/representations.py
+++ ayon_server/graphql/resolvers/representations.py
@@ -148,9 +148,11 @@ async def get_representations(
             "version_id",
             "files",
             "attrib",
+            "data",
             "traits",
             "status",
             "tags",
+            "active",
             "created_at",
             "updated_at",
         ]
