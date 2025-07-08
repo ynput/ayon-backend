f79330ad5306306158e4b9eb21a0c8a4002f40c7
Martastain <martas@imm.cz>
2025-07-08 15:20:03 +0200

    fix: invalidate settings cache upon dev bundle update

--- ayon_server/events/default_hooks.py
+++ ayon_server/events/default_hooks.py
@@ -12,11 +12,16 @@ if TYPE_CHECKING:
 
 
 async def on_settings_changed(event: "EventModel"):
-    _ = event
-    logger.trace("Clearing all-settings cache")
-    await Redis.delete_ns("all-settings")
+    if event.topic == "settings.changed" or event.summary.get("isDev", False):
+        # If the event is about settings change ,
+        # or a dev bundle has changed (because it affects addon list)
+        # we clear the cache for all settings.
+        logger.trace("Clearing all-settings cache")
+        await Redis.delete_ns("all-settings")
 
 
 DEFAULT_HOOKS: list[tuple[str, HandlerType, bool]] = [
     ("settings.changed", on_settings_changed, False),
+    ("bundle.created", on_settings_changed, False),
+    ("bundle.updated", on_settings_changed, False),
 ]
