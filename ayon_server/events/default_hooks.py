2925e48451b35c181e7f2b793e77e6d00c256fb1
Martastain <martas@imm.cz>
2025-07-10 12:59:53 +0200

    fix: clear setting cache on all bundle updates

--- ayon_server/events/default_hooks.py
+++ ayon_server/events/default_hooks.py
@@ -11,17 +11,13 @@ if TYPE_CHECKING:
     from .base import EventModel
 
 
-async def on_settings_changed(event: "EventModel"):
-    if event.topic == "settings.changed" or event.summary.get("isDev", False):
-        # If the event is about settings change ,
-        # or a dev bundle has changed (because it affects addon list)
-        # we clear the cache for all settings.
-        logger.trace("Clearing all-settings cache")
-        await Redis.delete_ns("all-settings")
+async def clear_settings_cache(event: "EventModel"):
+    logger.trace("Clearing all-settings cache")
+    await Redis.delete_ns("all-settings")
 
 
 DEFAULT_HOOKS: list[tuple[str, HandlerType, bool]] = [
-    ("settings.changed", on_settings_changed, False),
-    ("bundle.created", on_settings_changed, False),
-    ("bundle.updated", on_settings_changed, False),
+    ("settings.changed", clear_settings_cache, False),
+    ("bundle.created", clear_settings_cache, False),
+    ("bundle.updated", clear_settings_cache, False),
 ]
