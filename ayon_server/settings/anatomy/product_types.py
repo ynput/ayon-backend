d985b3d58052a17bc7885e7bd88fbb3f3d1b0edb
Martastain <martas@imm.cz>
2025-07-16 16:01:20 +0200

    refactor: use type ProductTypeOverride for product type customizations

--- ayon_server/settings/anatomy/product_types.py
+++ ayon_server/settings/anatomy/product_types.py
@@ -4,7 +4,7 @@ from ayon_server.settings.common import BaseSettingsModel
 from ayon_server.settings.settings_field import SettingsField
 
 
-class ProductType(BaseSettingsModel):
+class ProductTypeOverride(BaseSettingsModel):
     """Product type customization settings."""
 
     _layout = "compact"
@@ -19,31 +19,31 @@ class ProductBaseType(BaseSettingsModel):
     color: Annotated[str, SettingsField(title="Color", widget="color")] = "#cccccc"
     icon: Annotated[str, SettingsField(title="Icon", widget="icon")] = "deployed_code"
     product_types: Annotated[
-        list[ProductType],
+        list[ProductTypeOverride],
         SettingsField(default_factory=list, title="Product Types"),
     ]
 
 
 default_product_types = [
-    ProductType(name="image", icon="imagesmode"),
-    ProductType(name="render", icon="photo_library"),
-    ProductType(name="review", icon="photo_library"),
-    ProductType(name="plate", icon="camera_roll"),
-    ProductType(name="camera", icon="videocam"),
-    ProductType(name="model", icon="language"),
-    ProductType(name="texture", icon="texture"),
-    ProductType(name="look", icon="ev_shadow"),
-    ProductType(name="rig", icon="accessibility"),
-    ProductType(name="animation", icon="directions_run"),
-    ProductType(name="cache", icon="animation"),
-    ProductType(name="layout", icon="nature_people"),
-    ProductType(name="setdress", icon="forest"),
-    ProductType(name="groom", icon="content_cut"),
-    ProductType(name="matchmove", icon="switch_video"),
-    ProductType(name="vdbcache", icon="local_fire_department"),
-    ProductType(name="lightrig", icon="wb_incandescent"),
-    ProductType(name="lut", icon="opacity"),
-    ProductType(name="workfile", icon="home_repair_service"),
+    ProductTypeOverride(name="image", icon="imagesmode"),
+    ProductTypeOverride(name="render", icon="photo_library"),
+    ProductTypeOverride(name="review", icon="photo_library"),
+    ProductTypeOverride(name="plate", icon="camera_roll"),
+    ProductTypeOverride(name="camera", icon="videocam"),
+    ProductTypeOverride(name="model", icon="language"),
+    ProductTypeOverride(name="texture", icon="texture"),
+    ProductTypeOverride(name="look", icon="ev_shadow"),
+    ProductTypeOverride(name="rig", icon="accessibility"),
+    ProductTypeOverride(name="animation", icon="directions_run"),
+    ProductTypeOverride(name="cache", icon="animation"),
+    ProductTypeOverride(name="layout", icon="nature_people"),
+    ProductTypeOverride(name="setdress", icon="forest"),
+    ProductTypeOverride(name="groom", icon="content_cut"),
+    ProductTypeOverride(name="matchmove", icon="switch_video"),
+    ProductTypeOverride(name="vdbcache", icon="local_fire_department"),
+    ProductTypeOverride(name="lightrig", icon="wb_incandescent"),
+    ProductTypeOverride(name="lut", icon="opacity"),
+    ProductTypeOverride(name="workfile", icon="home_repair_service"),
 ]
 
 
@@ -51,7 +51,7 @@ class ProductTypes(BaseSettingsModel):
     """Product types customization settings."""
 
     default: Annotated[
-        list[ProductType],
+        list[ProductTypeOverride],
         SettingsField(
             title="Default product types",
             default_factory=lambda: default_product_types,
