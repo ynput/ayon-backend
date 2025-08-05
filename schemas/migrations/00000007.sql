f869ec8c1e54b5ab911960e75ce336d48c6ade4f
Martastain <martas@imm.cz>
2025-08-05 09:26:27 +0200

    fix: temporary migration for created/updated at columns

--- schemas/migrations/00000007.sql
+++ schemas/migrations/00000007.sql
@@ -31,7 +31,9 @@ BEGIN
         personal BOOLEAN NOT NULL DEFAULT TRUE,
 
         access JSONB NOT NULL DEFAULT '{}'::JSONB,
-        data JSONB NOT NULL DEFAULT '{}'::JSONB
+        data JSONB NOT NULL DEFAULT '{}'::JSONB,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
       );
 
       CREATE UNIQUE INDEX IF NOT EXISTS unique_personal_view ON views(view_type, owner) WHERE personal;
@@ -44,3 +46,27 @@ BEGIN
   END LOOP;
   RETURN;
 END $$;
+
+
+
+-- TODO: can be removed in 1.11.0
+
+DO $$
+DECLARE rec RECORD;
+BEGIN
+    FOR rec IN SELECT DISTINCT nspname FROM pg_namespace WHERE nspname LIKE 'project_%'
+    LOOP
+        BEGIN
+          EXECUTE 'SET LOCAL search_path TO ' || quote_ident(rec.nspname);
+          ALTER TABLE IF EXISTS views ADD COLUMN IF NOT EXISTS created_at TIMESTAMPTZ NOT NULL DEFAULT NOW();
+          ALTER TABLE IF EXISTS views ADD COLUMN IF NOT EXISTS updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW();
+        EXCEPTION
+          WHEN OTHERS THEN
+             RAISE WARNING 'Skipping schema % due to error: %', rec.nspname, SQLERRM;
+        END;
+    END LOOP;
+    RETURN;
+END $$;
+
+ALTER TABLE IF EXISTS public.views ADD COLUMN IF NOT EXISTS created_at TIMESTAMPTZ NOT NULL DEFAULT NOW();
+ALTER TABLE IF EXISTS public.views ADD COLUMN IF NOT EXISTS updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW();
