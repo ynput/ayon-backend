4447351bee2b692ca0c9dab56a5c6bb27130a7aa
Martastain <martas@imm.cz>
2025-08-08 11:48:11 +0200

    fix: cli modules sanity check

--- ayon_server/__main__.py
+++ ayon_server/__main__.py
@@ -7,7 +7,8 @@ from ayon_server.logging import logger
 
 CLI_PLUGINS_DIRS = [
     "cli",
-    "/ayon-server-cli" "/storage/ayon-server-cli",
+    "/ayon-server-cli",
+    "/storage/ayon-server-cli",
 ]
 
 
@@ -17,6 +18,11 @@ def main() -> None:
             continue
         sys.path.insert(0, plugin_dir)
         for module_name in sorted(os.listdir(plugin_dir)):
+            module_path = os.path.join(plugin_dir, module_name)
+            init_path = os.path.join(module_path, "__init__.py")
+            if not os.path.isdir(module_path) or not os.path.isfile(init_path):
+                continue
+
             try:
                 _ = importlib.import_module(module_name)
             except ImportError:
