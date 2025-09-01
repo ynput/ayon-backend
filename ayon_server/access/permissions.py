bd9a1db757bc38c710713a755ab5fc27fccd6599
Martin Wacker <martas@imm.cz>
2025-09-01 14:44:30 +0200

    Update ayon_server/access/permissions.py
    Co-authored-by: Luke Inderwick <49156310+Innders@users.noreply.github.com>

--- ayon_server/access/permissions.py
+++ ayon_server/access/permissions.py
@@ -188,7 +188,7 @@ class ProjectAdvancedPermissions(BaseSettingsModel):
     show_sibling_tasks: bool = SettingsField(
         True,
         title="Show sibling tasks",
-        description="Show sibling tasks when access mode is 'Assigned'",
+        description="If a user can access a task through the 'Assigned' permission, enabling this will also show all sibling tasks in the same folder. When disabled, only the assigned task is visible.",
     )
 
 
