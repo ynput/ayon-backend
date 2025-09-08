3967df8fbfb71d474d898541a2b557f148c4186a
Martastain <martas@imm.cz>
2025-09-08 12:17:00 +0200

    refactor: guests api

--- api/projects/guests.py
+++ api/projects/guests.py
@@ -2,13 +2,13 @@ from typing import Annotated
 
 from ayon_server.api.dependencies import CurrentUser, ProjectName
 from ayon_server.entities import ProjectEntity
-from ayon_server.helpers.external_users import ExternalUsers, ExternalUserStatus
+from ayon_server.helpers.guest_users import GuestUsers, GuestUserStatus
 from ayon_server.types import Field, OPModel
 
 from .router import router
 
 
-class ExternalUserModel(OPModel):
+class GuestUserModel(OPModel):
     email: Annotated[
         str,
         Field(
@@ -26,7 +26,7 @@ class ExternalUserModel(OPModel):
     ] = None
 
     status: Annotated[
-        ExternalUserStatus,
+        GuestUserStatus,
         Field(
             title="Status",
             example="pending",
@@ -34,65 +34,65 @@ class ExternalUserModel(OPModel):
     ] = "pending"
 
 
-class ExternalUsersListModel(OPModel):
+class GuestUsersListModel(OPModel):
     users: Annotated[
-        list[ExternalUserModel],
+        list[GuestUserModel],
         Field(
-            title="External users",
+            title="Guest users",
             default_factory=list,
         ),
     ]
 
 
-@router.get("/projects/{project_name}/externalUsers")
-async def list_external_users(
+@router.get("/projects/{project_name}/guests")
+async def list_guest_users(
     user: CurrentUser, project_name: ProjectName
-) -> ExternalUsersListModel:
+) -> GuestUsersListModel:
     """Retrieve a project statistics by its name."""
 
     user.check_permissions("project.access", project_name)
     project = await ProjectEntity.load(name=project_name)
-    external_users = project.data.get("externalUsers", {})
-    emails = sorted(external_users.keys())
+    guest_users = project.data.get("guestUsers", {})
+    emails = sorted(guest_users.keys())
     result = []
     for email in emails:
-        user_data = external_users[email]
+        user_data = guest_users[email]
         result.append(
-            ExternalUserModel(
+            GuestUserModel(
                 email=email,
                 full_name=user_data.get("fullName"),
                 status=user_data.get("status", "pending"),
             )
         )
-    return ExternalUsersListModel(users=result)
+    return GuestUsersListModel(users=result)
 
 
-class AddExternalUserModel(OPModel):
+class AddGuestUserModel(OPModel):
     email: Annotated[str, Field(title="Email", example="foo.bar@example.com")]
     full_name: Annotated[str | None, Field(title="Full name", example="Foo Bar")] = None
 
 
-@router.post("/projects/{project_name}/externalUsers")
-async def add_external_user(
+@router.post("/projects/{project_name}/guests")
+async def add_guest_user(
     user: CurrentUser,
     project_name: ProjectName,
-    payload: AddExternalUserModel,
+    payload: AddGuestUserModel,
 ):
     user.check_permissions("project.access", project_name, write=True)
 
-    await ExternalUsers.add(
+    await GuestUsers.add(
         email=payload.email,
         project_name=project_name,
         full_name=payload.full_name,
     )
 
 
-@router.delete("/projects/{project_name}/externalUsers/{email}")
-async def remove_external_user(
+@router.delete("/projects/{project_name}/guests/{email}")
+async def remove_guest_user(
     user: CurrentUser,
     project_name: ProjectName,
     email: str,
 ):
     user.check_permissions("project.access", project_name, write=True)
 
-    await ExternalUsers.remove(email=email, project_name=project_name)
+    await GuestUsers.remove(email=email, project_name=project_name)
