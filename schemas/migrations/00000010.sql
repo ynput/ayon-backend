94eece3a833e0b881c0f2713d2652972a617893e
Martastain <martas@imm.cz>
2025-10-21 15:26:46 +0200

    fix: migration script

--- schemas/migrations/00000010.sql
+++ schemas/migrations/00000010.sql
@@ -41,35 +41,68 @@ FOR rec IN
 END $$;
 
 --
+-- Add product_base_type to products table in project schemas if it doesn't exist
+--
+
+DO $$
+DECLARE rec RECORD;
+BEGIN
+  FOR rec IN SELECT t.table_schema, t.table_name
+    FROM information_schema.tables t
+    LEFT JOIN information_schema.columns c
+    ON t.table_schema = c.table_schema
+    AND t.table_name = c.table_name
+    AND c.column_name = 'product_base_type'
+    WHERE t.table_schema LIKE 'project_%'
+    AND t.table_name = 'products'
+    GROUP BY t.table_schema, t.table_name
+    HAVING COUNT(c.column_name) = 0
+    LOOP
+        BEGIN
+          RAISE WARNING 'Adding product_base_type and updated_by to %.%', rec.table_schema, rec.table_name;
+          EXECUTE 'SET LOCAL search_path TO ' || quote_ident(rec.table_schema);
+          -- TODO: Uncomment once we are sure
+          -- EXECUTE 'ALTER TABLE' || quote_ident(rec.table_name) || 'ADD COLUMN IF NOT EXISTS product_base_type VARCHAR;';
+        EXCEPTION
+          WHEN OTHERS THEN
+             RAISE WARNING 'Skipping schema % due to error: %', rec.table_schema, SQLERRM;
+        END;
+    END LOOP;
+    RETURN;
+END $$;
+
+--
 -- Add created_by and updated_by to project level entities
 --
 
 DO $$
 DECLARE rec RECORD;
 BEGIN
-    FOR rec IN SELECT DISTINCT p.nspname FROM pg_namespace p
+  FOR rec IN SELECT t.table_schema, t.table_name
+    FROM information_schema.tables t
     LEFT JOIN information_schema.columns c
-    ON p.nspname = c.table_schema
-    AND c.table_name IN (
+    ON t.table_schema = c.table_schema
+    AND t.table_name = c.table_name
+    AND c.column_name IN ('created_by', 'updated_by')
+    WHERE t.table_schema LIKE 'project_%'
+    AND t.table_name IN (
       'folders',
       'tasks',
       'products',
       'versions',
-      'representations',
+      'representations'
     )
-    WHERE p.nspname LIKE 'project_%'
-    AND c.table_name IS NULL
-    
+    GROUP BY t.table_schema, t.table_name
     LOOP
         BEGIN
-          RAISE WARNING 'Adding created_by and updated_by to %.%', rec.nspname, rec.table_name;
+          RAISE WARNING 'Adding created_by and updated_by to %.%', rec.table_schema, rec.table_name;
+          EXECUTE 'SET LOCAL search_path TO ' || quote_ident(rec.table_schema);
           -- TODO: Uncomment once we are sure
-          -- EXECUTE 'SET LOCAL search_path TO ' || quote_ident(rec.nspname);
           -- EXECUTE 'ALTER TABLE' || quote_ident(rec.table_name) || 'ADD COLUMN IF NOT EXISTS created_by VARCHAR;';
           -- EXECUTE 'ALTER TABLE' || quote_ident(rec.table_name) || 'ADD COLUMN IF NOT EXISTS updated_by VARCHAR;';
         EXCEPTION
           WHEN OTHERS THEN
-             RAISE WARNING 'Skipping schema % due to error: %', rec.nspname, SQLERRM;
+             RAISE WARNING 'Skipping schema % due to error: %', rec.table_schema, SQLERRM;
         END;
     END LOOP;
     RETURN;
