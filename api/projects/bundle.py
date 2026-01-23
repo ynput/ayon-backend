91de2f9fddd8eea00037877876b8b7ccfe6caa01
Martastain <martas@imm.cz>
2026-01-23 11:45:19 +0100

    feat: freeze project bundle

--- api/projects/bundle.py
+++ api/projects/bundle.py
@@ -1,10 +1,19 @@
+from typing import Annotated, Literal
+
+from fastapi import Query
+from pydantic import Field
+
 from ayon_server.api.dependencies import CurrentUser, ProjectName, Sender, SenderType
 from ayon_server.api.responses import EmptyResponse
+from ayon_server.bundles.project_bundles import (
+    freeze_project_bundle,
+    unfreeze_project_bundle,
+)
 from ayon_server.entities import ProjectEntity
 from ayon_server.exceptions import ForbiddenException
 from ayon_server.lib.postgres import Postgres
 from ayon_server.lib.redis import Redis
-from ayon_server.types import OPModel
+from ayon_server.types import OPModel, Platform
 
 from .router import router
 
@@ -14,16 +23,18 @@ class ProjectBundleModel(OPModel):
     staging: str | None = None
 
 
-@router.post("/projects/{project_name}/bundles", status_code=204)
-async def set_project_bundle(
+@router.post("/projects/{project_name}/bundles", status_code=204, deprecated=True)
+async def set_project_bundles(
     user: CurrentUser,
     project_name: ProjectName,
     payload: ProjectBundleModel,
     sender: Sender,
     sender_type: SenderType,
 ) -> EmptyResponse:
-    """Set a project anatomy."""
+    """Set project bundle
 
+    Deprecated: Use the freeze_project_bundle function instead.
+    """
     _ = sender, sender_type
 
     if not user.is_manager:
@@ -40,5 +51,84 @@ async def set_project_bundle(
             project.data["bundle"] = bundle_data
         await project.save()
         await Redis.delete_ns("all-settings")
+
+    return EmptyResponse()
+
+
+#
+# Project bundle management
+#
+
+BundleVariant = Annotated[
+    Literal["production", "staging"],
+    Query(title="Bundle Variant"),
+]
+
+
+class SetProjectBundleRequest(OPModel):
+    addons: Annotated[
+        dict[str, str | None],
+        Field(
+            title="Addons",
+            description=(
+                "Dictionary of addon names and their versions. "
+                "Use `null` to disable an addon."
+            ),
+        ),
+    ]
+
+    installer_version: Annotated[
+        str | None,
+        Field(None, title="Installer Version"),
+    ] = None
+
+    dependency_packages: Annotated[
+        dict[Platform, str | None] | None,
+        Field(title="Platform"),
+    ] = None
+
+
+@router.post("/projects/{project_name}/bundle")
+async def set_project_bundle(
+    user: CurrentUser,
+    project_name: ProjectName,
+    sender: Sender,
+    sender_type: SenderType,
+    payload: SetProjectBundleRequest,
+    variant: BundleVariant = "production",
+) -> EmptyResponse:
+    """Set project bundle"""
+    if not user.is_manager:
+        raise ForbiddenException("Only managers can set project bundle")
+
+    _ = sender, sender_type
+
+    return await freeze_project_bundle(
+        project_name,
+        variant=variant,
+        addons=payload.addons,
+        installer_version=payload.installer_version,
+        dependency_packages=payload.dependency_packages,
+    )
+
+
+@router.delete("/projects/{project_name}/bundle", status_code=204)
+async def unset_project_bundle(
+    user: CurrentUser,
+    project_name: ProjectName,
+    variant: Literal["production", "staging"],
+    sender: Sender,
+    sender_type: SenderType,
+) -> EmptyResponse:
+    """Unset project bundle"""
+    if not user.is_manager:
+        raise ForbiddenException("Only managers can unset project bundle")
+
+    _ = sender, sender_type
+
+    await unfreeze_project_bundle(
+        project_name,
+        variant=variant,
+    )
 
     return EmptyResponse()
---
+++ ayon_server/bundles/project_bundles.py
@@ -0,0 +1,176 @@
+from typing import Literal
+
+from ayon_server.addons.library import AddonLibrary
+from ayon_server.entities import ProjectEntity
+from ayon_server.exceptions import BadRequestException
+from ayon_server.lib.postgres import Postgres
+from ayon_server.lib.redis import Redis
+from ayon_server.types import Platform
+
+
+def get_project_bundle_name(
+    project_name: str,
+    variant: Literal["production", "staging"],
+) -> str:
+    """Get project bundle name based on project name and variant"""
+    return f"__project__{project_name}__{variant}"
+
+
+async def process_addon_settings(
+    addon_name: str,
+    addon_version: str,
+    project_name: str,
+    variant: Literal["production", "staging"],
+) -> None:
+    """Process addon settings for the given project and variant."""
+
+    #
+    # Sanity checks
+    # If the addon does not exist or does not allow overrides,
+    # raise an exception, so the entire transaction is rolled back.
+    #
+
+    # if addon is not found, it raises NotFoundException,
+    addon = AddonLibrary.addon(addon_name, addon_version)
+
+    if not addon.project_can_override_addon_version:
+        raise BadRequestException(
+            f"Addon {addon_name} version {addon_version} "
+            f"does not allow project overrides"
+        )
+
+    # Get the "full" project settings (studio settings + overrides)
+
+    project_settings = await addon.get_project_settings(project_name, variant)
+    # TODO: load original overrides so, we can create a change event
+
+    # Save the entire object as project settings overrides
+
+    update_query = """
+        INSERT INTO settings (addon_name, addon_version, variant, data)
+        VALUES ($1, $2, $3, $4)
+        ON CONFLICT (addon_name, addon_version, variant) DO UPDATE SET
+            data = EXCLUDED.data
+    """
+
+    await Postgres.execute(
+        update_query,
+        addon_name,
+        addon_version,
+        variant,
+        project_settings,
+    )
+
+
+async def freeze_project_bundle(
+    project_name: str,
+    *,
+    variant: Literal["production", "staging"] = "production",
+    addons: dict[str, str | None] | None = None,
+    installer_version: str | None = None,
+    dependency_packages: dict[Platform, str | None] | None = None,
+) -> None:
+    """Freeze project bundle
+
+    - ensures that the bundle for the given project and variant exists
+    - update the bundle with the given data
+    - copies all project settings to the project and enforce them as overrides
+    - sets the project bundle in project data
+
+    """
+
+    addons = addons or {}
+    dependency_packages = dependency_packages or {}
+
+    bundle_name = get_project_bundle_name(project_name, variant)
+    is_production = variant == "production"
+    is_staging = variant == "staging"
+
+    bundle_data = {
+        "addons": addons,
+        "installer_version": installer_version,
+        "dependency_packages": dependency_packages,
+        "is_project": True,
+    }
+
+    query = """
+        INSERT INTO public.bundles (name, is_production, is_staging, data)
+        VALUES ($1, $2, $3, $4)
+        ON CONFLICT (name) DO UPDATE SET
+            is_production = EXCLUDED.is_production,
+            is_staging = EXCLUDED.is_staging,
+            data = EXCLUDED.data
+    """
+
+    async with Postgres.transaction():
+        # set project schema explicitly,
+        # all operations that use public schema in this transaction
+        # must use `public.` prefix in their queries
+
+        await Postgres.set_project_schema(project_name)
+
+        # Ensure well-known project bundle exists / is updated
+
+        await Postgres.execute(
+            query,
+            bundle_name,
+            is_production,
+            is_staging,
+            bundle_data,
+        )
+
+        # Enforce all overrides for project settings
+
+        for addon_name, addon_version in addons.items():
+            if addon_version is None:
+                continue
+
+            await process_addon_settings(
+                addon_name,
+                addon_version,
+                project_name,
+                variant,
+            )
+
+        # Save bundle reference to project data
+
+        project = await ProjectEntity.load(project_name, for_update=True)
+        bundle_info = project.data.get("bundle", {})
+        bundle_info[variant] = bundle_name
+        project.data["bundle"] = bundle_info
+        await project.save()
+
+
+async def unfreeze_project_bundle(
+    project_name: str,
+    variant: Literal["production", "staging"],
+) -> None:
+    """Unfreeze project bundle
+
+    - removes the bundle for the given project and variant
+    - removes the project bundle from project data
+    """
+    bundle_name = get_project_bundle_name(project_name, variant)
+
+    async with Postgres.transaction():
+        #
+        # Unset project bundle in project data
+        #
+
+        project = await ProjectEntity.load(project_name, for_update=True)
+        bundle_info = project.data.get("bundle", {})
+        bundle_info.pop(variant, None)
+        if not bundle_info:
+            project.data.pop("bundle", None)
+        else:
+            project.data["bundle"] = bundle_info
+        await project.save()
+
+        #
+        # Remove the bundle
+        #
+
+        delete_query = "DELETE FROM bundles WHERE name = $1"
+        await Postgres.execute(delete_query, bundle_name)
+
+    await Redis.delete_ns("all-settings")
