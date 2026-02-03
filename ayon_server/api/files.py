8dc11e9cd2a5f66086209b7938c422a19fda867e
Martastain <martas@imm.cz>
2026-02-03 14:41:59 +0100

    fix: better path sanitation

--- ayon_server/api/files.py
+++ ayon_server/api/files.py
@@ -1,4 +1,6 @@
 import os
+import pathlib
+from contextlib import suppress
 
 import aiofiles
 from fastapi import Request, Response
@@ -10,19 +12,27 @@ from ayon_server.helpers.mimetypes import guess_mime_type
 from ayon_server.helpers.statistics import update_traffic_stats
 
 
-async def handle_upload(request: Request, target_path: str) -> int:
+async def handle_upload(request: Request, target_path: str | pathlib.Path) -> int:
     """Store raw body from the request to a file.
 
-    Returns file size in bytes.
+    Important:
+
+    Returns file size in bytes. It is a responsibility of the caller to validate
+    whether the file is acceptable and the user has permissions to upload to the
+    target path.
     """
 
-    directory, _ = os.path.split(target_path)
+    target_path = pathlib.Path(target_path).resolve()
+    directory = target_path.parent
 
-    if not os.path.isdir(directory):
+    if not directory.exists():
         try:
             os.makedirs(directory)
         except Exception as e:
             raise AyonException(f"Failed to create directory: {e}") from e
+
+    elif not directory.is_dir():
+        raise AyonException("Target directory is not a directory")
 
     i = 0
     try:
@@ -30,46 +40,61 @@ async def handle_upload(request: Request, target_path: str) -> int:
             async for chunk in request.stream():
                 await f.write(chunk)
                 i += len(chunk)
+
     except Exception as e:
-        try:
-            os.remove(target_path)
-        except Exception:
-            pass
+        with suppress(Exception):
+            target_path.unlink()
         raise AyonException(f"Failed to write file: {e}") from e
+
     finally:
         if i:
             await update_traffic_stats("ingress", i)
 
     if i == 0:
-        try:
-            os.remove(target_path)
-        except Exception:
-            pass
+        with suppress(Exception):
+            target_path.unlink()
         raise BadRequestException("Empty file")
 
     return i
 
 
 async def handle_download(
-    path: str,
+    path: str | pathlib.Path,
     media_type: str = "application/octet-stream",
     filename: str | None = None,
     content_disposition_type: str = "attachment",
+    root_dir: str | pathlib.Path | None = None,
 ) -> FileResponse:
-    _, _filename = os.path.split(path)
-    if filename is None:
-        filename = _filename
-    if not os.path.isfile(path):
-        raise NotFoundException(f"No such file {filename}")
+    """Serve a file from the given path.
+
+    Important: path must be validated before calling this function!
+    File existence is checked here, but since we need to be able any file
+    and there's no single root directory, we cannot do path validation,
+    if only the path is given. It is a responsibility of the caller to ensure
+    that the path is safe to serve.
+
+    If `root_dir` is provided, the path is resolved relative to the root and
+    validated to be under the root.
+    """
+
+    requested_path = pathlib.Path(path).resolve()
+    if root_dir is not None:
+        root_path = pathlib.Path(root_dir).resolve()
+        if not requested_path.is_relative_to(root_path):
+            raise NotFoundException("Invalid file path")
+
+    if not requested_path.is_file():
+        raise NotFoundException("File not found")
+
+    filename = requested_path.name if filename is None else filename
+    filesize = requested_path.stat().st_size
 
     return FileResponse(
         path,
         media_type=media_type,
         filename=filename,
         content_disposition_type=content_disposition_type,
-        background=BackgroundTask(
-            update_traffic_stats, "egress", os.path.getsize(path)
-        ),
+        background=BackgroundTask(update_traffic_stats, "egress", filesize),
     )
 
 
