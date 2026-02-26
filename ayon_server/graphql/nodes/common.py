5913e2ab1cf210ecb580ca7823e971328f603177
Roy Nieterau <roy_nieterau@hotmail.com>
2026-02-26 23:01:29 +0100

    Expose `data` for the LinkEdge GraphQL response

--- ayon_server/graphql/nodes/common.py
+++ ayon_server/graphql/nodes/common.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import Any, Optional
 
 import strawberry
+from strawberry.scalars import JSON
 
 from ayon_server.entities import UserEntity
 from ayon_server.exceptions import AyonException
@@ -36,6 +37,7 @@ class LinkEdge(BaseEdge):
     description: str = strawberry.field(default="")
     author: str | None = strawberry.field(default=None)
     cursor: str | None = strawberry.field(default=None)
+    data: JSON | None = strawberry.field(default=None)
 
     @strawberry.field(description="Linked node")
     async def node(self, info: Info) -> Optional["BaseNode"]:
