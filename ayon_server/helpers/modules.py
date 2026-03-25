6513ba09ad7bf34788050393d42543e52d66976b
Martastain <martas@imm.cz>
2026-03-25 08:19:17 +0100

    chore: remove legacy code

--- ayon_server/helpers/modules.py
+++ ayon_server/helpers/modules.py
@@ -8,18 +8,6 @@ from typing import TypeVar
 T = TypeVar("T", bound=type)
 
 
-# def import_module(name: str, path: str) -> ModuleType:
-#     if (spec := importlib.util.spec_from_file_location(name, path)) is None:
-#         raise ModuleNotFoundError(f"Module {name} not found")
-#     if (module := importlib.util.module_from_spec(spec)) is None:
-#         raise ImportError(f"Module {name} cannot be imported")
-#     if spec.loader is None:
-#         raise ImportError(f"Module {name} cannot be imported. No loader found.")
-#     sys.modules[spec.name] = module
-#     spec.loader.exec_module(module)
-#     return module
-
-
 def import_module(name: str, path: str) -> ModuleType:
     """
     Imports a plugin module into a unique namespace to prevent collisions.
