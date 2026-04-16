f8531ed5940c64e861c98cbb432f19a70d83f806
Martastain <martas@imm.cz>
2026-04-16 13:58:56 +0200

    fix: access to user enum

--- ayon_server/enum/resolvers/enum_users.py
+++ ayon_server/enum/resolvers/enum_users.py
@@ -3,6 +3,7 @@ from typing import Any
 from ayon_server.config import ayonconfig
 from ayon_server.enum.base_resolver import BaseEnumResolver
 from ayon_server.enum.enum_item import EnumItem
+from ayon_server.exceptions import ForbiddenException
 from ayon_server.lib.postgres import Postgres
 from ayon_server.models import IconModel
 
@@ -23,33 +24,62 @@ class UsersEnumResolver(BaseEnumResolver):
 
         project_name = context.get("project_name")
         current_user = context.get("user")
-        if current_user and not current_user.is_manager and not project_name:
-            # Non-managers can only query users within a project
-            # they have access to.
-            return []
+        if current_user and not current_user.is_manager:
+            if not project_name:
+                # Non-managers can only query users within a project
+                # they have access to.
+                raise ForbiddenException("Project name is required for non-admins")
+
+            current_user_ags = current_user.data.get("accessGroups", {}).get(
+                project_name, []
+            )
+            if not current_user_ags:
+                raise ForbiddenException("You don't have access to this project")
 
         skip_if_not_ag = None
-        if ayonconfig.limit_user_visibility and current_user:
+        if (
+            ayonconfig.limit_user_visibility
+            and current_user
+            and not current_user.is_manager
+        ):
             skip_if_not_ag = current_user.data.get("accessGroups", {}).get(
                 project_name, []
             )
+
+        def should_show_user(udata: dict[str, Any]) -> bool:
+            is_admin = udata.get("isAdmin", False)
+            is_manager = udata.get("isManager", False)
+
+            if is_admin or is_manager:
+                return True
+
+            if not current_user:
+                return True
+
+            if not project_name:
+                return current_user.is_manager
+
+            # now we are in project scope
+
+            ags = udata.get("accessGroups", {}).get(project_name, [])
+            if not ags:
+                return False
+
+            if current_user.is_manager:
+                return True
+
+            if not skip_if_not_ag:
+                return True
+
+            return bool(set(ags).intersection(set(skip_if_not_ag)))
 
         async with Postgres.transaction():
             stmt = await Postgres.prepare(query)
             async for row in stmt.cursor():
                 name, attrib, udata = row
 
-                is_admin = udata.get("isAdmin", False)
-                is_manager = udata.get("isManager", False)
-
-                if not (is_admin or is_manager):
-                    ags = udata.get("accessGroups", {}).get(project_name, [])
-                    if not ags:
-                        continue
-
-                    if skip_if_not_ag is not None:
-                        if not set(ags).intersection(set(skip_if_not_ag)):
-                            continue
+                if not should_show_user(udata):
+                    continue
 
                 item = EnumItem(
                     value=name,
