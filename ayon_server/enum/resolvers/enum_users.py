eeeffeb2433cfe31474e8807fc663f1b88bb831c
Martastain <martas@imm.cz>
2026-04-16 15:16:53 +0200

    fix: simplified permission logic

--- ayon_server/enum/resolvers/enum_users.py
+++ ayon_server/enum/resolvers/enum_users.py
@@ -24,49 +24,57 @@ class UsersEnumResolver(BaseEnumResolver):
 
         project_name = context.get("project_name")
         current_user = context.get("user")
-        if current_user and not current_user.is_manager:
+
+        skip_if_not_ag = None
+        has_all_user_access = False
+
+        if not current_user:
+            # If there is no current user, we assume this is a system process
+            # that has access to all users.
+            has_all_user_access = True
+        else:
+            try:
+                current_user.check_permissions("studio.list_all_users")
+            except ForbiddenException:
+                # normal user without studio-wide access
+                if ayonconfig.limit_user_visibility:
+                    skip_if_not_ag = current_user.data.get("accessGroups", {}).get(
+                        project_name, []
+                    )
+            else:
+                has_all_user_access = True
+
+        if not has_all_user_access:
             if not project_name:
                 # Non-managers can only query users within a project
                 # they have access to.
-                raise ForbiddenException("Project name is required for non-admins")
+                raise ForbiddenException(
+                    "You don't have access to studio-wide user list"
+                )
 
+            assert current_user is not None  # for mypy
             current_user_ags = current_user.data.get("accessGroups", {}).get(
                 project_name, []
             )
             if not current_user_ags:
                 raise ForbiddenException("You don't have access to this project")
 
-        skip_if_not_ag = None
-        if (
-            ayonconfig.limit_user_visibility
-            and current_user
-            and not current_user.is_manager
-        ):
-            skip_if_not_ag = current_user.data.get("accessGroups", {}).get(
-                project_name, []
-            )
-
         def should_show_user(udata: dict[str, Any]) -> bool:
             is_admin = udata.get("isAdmin", False)
             is_manager = udata.get("isManager", False)
 
             if is_admin or is_manager:
-                return True
-
-            if not current_user:
+                # we always show admins and managers
                 return True
 
             if not project_name:
-                return current_user.is_manager
+                return has_all_user_access
 
             # now we are in project scope
 
             ags = udata.get("accessGroups", {}).get(project_name, [])
             if not ags:
                 return False
-
-            if current_user.is_manager:
-                return True
 
             if not skip_if_not_ag:
                 return True
