ed843efa35aed927232bd577cba0f2153b205495
Martastain <martas@imm.cz>
2026-05-18 16:21:55 +0200

    feat: user invite api

---
+++ api/users/invite.py
@@ -0,0 +1,120 @@
+import secrets
+import time
+from typing import Annotated
+
+from fastapi import Request
+
+from ayon_server.api.dependencies import CurrentUser, UserName
+from ayon_server.auth.models import LoginResponseModel
+from ayon_server.auth.session import Session
+from ayon_server.config.serverconfig import get_server_config
+from ayon_server.entities import UserEntity
+from ayon_server.exceptions import ForbiddenException
+from ayon_server.helpers.email import EmailTemplate
+from ayon_server.lib.postgres import Postgres
+from ayon_server.logging import logger
+from ayon_server.types import Field, OPModel
+from ayon_server.utils.server import server_url_from_request
+
+from .router import router
+
+TOKEN_TTL = 3600 * 24 * 7  # 7 days
+
+
+class InviteUserRequest(OPModel):
+    message: Annotated[
+        str | None,
+        Field(title="Message", example="Hi, please join our Ayon server!"),
+    ] = None
+
+
+@router.post("/{user_name}/invite")
+async def password_reset_request(
+    current_user: CurrentUser,
+    user_name: UserName,
+    payload: InviteUserRequest,
+    request: Request,
+):
+    if not current_user.is_manager:
+        raise ForbiddenException("Only managers and administrators can invite users")
+
+    user = await UserEntity.load(user_name)
+
+    token = secrets.token_urlsafe(32)
+    invite_request = {
+        "time": time.time(),
+        "token": token,
+    }
+
+    user.data["inviteRequest"] = invite_request
+    server_url = server_url_from_request(request)
+    server_config = await get_server_config()
+    studio_name = server_config.studio_name
+
+    tplvars = {
+        "reset_url": f"{server_url}/acceptInvite?token={token}",
+        "full_name": user.attrib.fullName or user.name,
+        "studio_name": studio_name,
+        "user_name": user.name,
+    }
+
+    template = EmailTemplate()
+    body = await template.render_template(
+        "user_invite.jinja",
+        tplvars,
+        base_url=server_url,
+    )
+
+    subject = f"You have been invited to join {studio_name} Ayon server"
+
+    await user.save()
+    await user.send_mail(
+        subject=subject,
+        html=body,
+    )
+    logger.info(f"Sent invitation email to {payload.email}")
+
+
+class AcceptInviteRequest(OPModel):
+    token: str = Field(..., title="Token")
+    password: str | None = Field(None, title="New password")
+
+
+@router.post("/acceptInvite")
+async def password_reset(
+    payload: AcceptInviteRequest, request: Request
+) -> LoginResponseModel:
+    query = "SELECT name, data FROM users WHERE data->'inviteRequest'->>'token' = $1"
+
+    ERROR_MESSAGE = "Invalid invite token or token has expired"
+
+    res = await Postgres.fetchrow(query, payload.token)
+    if res is None:
+        logger.error("Attempted accept invite using invalid token")
+        raise ForbiddenException(ERROR_MESSAGE)
+
+    user_name = res["name"]
+    user_data = res["data"]
+
+    invite_request = user_data.get("inviteRequest", {})
+    invite_request_time = invite_request.get("time") or 0
+
+    if time.time() - invite_request_time > TOKEN_TTL:
+        raise ForbiddenException(ERROR_MESSAGE)
+
+    if payload.password is None:
+        # just checking whether the token is valid
+        # we don't set the password
+        return LoginResponseModel(detail="Token is valid")
+
+    user = await UserEntity.load(user_name)
+    user.data["inviteRequest"] = None
+    user.set_password(payload.password, complexity_check=True)
+    await user.save()
+
+    session = await Session.create(user, request=request)
+    return LoginResponseModel(
+        detail="Password changed",
+        token=session.token,
+        user=session.user,
+    )
