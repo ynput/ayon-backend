e6e28221041b9f59ca1f783fafdf5802ba343a8c
petrk <petr.kalis@gmail.com>
2026-06-02 13:11:15 +0200

    Added field_stats to all Connections
    Currently implemented for FoldersConnection

--- ayon_server/graphql/connections.py
+++ ayon_server/graphql/connections.py
@@ -14,57 +14,67 @@ from ayon_server.graphql.edges import (
     VersionEdge,
     WorkfileEdge,
 )
-from ayon_server.graphql.types import BaseConnection
+from ayon_server.graphql.types import BaseConnection, ColumnStats
 
 
 @strawberry.type
 class ProjectsConnection(BaseConnection):
     edges: list[ProjectEdge] = strawberry.field(default_factory=list)
+    field_stats: list[ColumnStats] = strawberry.field(default_factory=list)
 
 
 @strawberry.type
 class UsersConnection(BaseConnection):
     edges: list[UserEdge] = strawberry.field(default_factory=list)
+    field_stats: list[ColumnStats] = strawberry.field(default_factory=list)
 
 
 @strawberry.type
 class FoldersConnection(BaseConnection):
     edges: list[FolderEdge] = strawberry.field(default_factory=list)
+    field_stats: list[ColumnStats] = strawberry.field(default_factory=list)
 
 
 @strawberry.type
 class TasksConnection(BaseConnection):
     edges: list[TaskEdge] = strawberry.field(default_factory=list)
+    field_stats: list[ColumnStats] = strawberry.field(default_factory=list)
 
 
 @strawberry.type
 class ProductsConnection(BaseConnection):
     edges: list[ProductEdge] = strawberry.field(default_factory=list)
+    field_stats: list[ColumnStats] = strawberry.field(default_factory=list)
 
 
 @strawberry.type
 class VersionsConnection(BaseConnection):
     edges: list[VersionEdge] = strawberry.field(default_factory=list)
+    field_stats: list[ColumnStats] = strawberry.field(default_factory=list)
 
 
 @strawberry.type
 class RepresentationsConnection(BaseConnection):
     edges: list[RepresentationEdge] = strawberry.field(default_factory=list)
+    field_stats: list[ColumnStats] = strawberry.field(default_factory=list)
 
 
 @strawberry.type
 class WorkfilesConnection(BaseConnection):
     edges: list[WorkfileEdge] = strawberry.field(default_factory=list)
+    field_stats: list[ColumnStats] = strawberry.field(default_factory=list)
 
 
 @strawberry.type
 class EventsConnection(BaseConnection):
     edges: list[EventEdge] = strawberry.field(default_factory=list)
+    field_stats: list[ColumnStats] = strawberry.field(default_factory=list)
 
 
 @strawberry.type
 class ActivitiesConnection(BaseConnection):
     edges: list[ActivityEdge] = strawberry.field(default_factory=list)
+    field_stats: list[ColumnStats] = strawberry.field(default_factory=list)
 
 
 # @strawberry.type
@@ -75,8 +85,10 @@ class ActivitiesConnection(BaseConnection):
 @strawberry.type
 class KanbanConnection(BaseConnection):
     edges: list[KanbanEdge] = strawberry.field(default_factory=list)
+    field_stats: list[ColumnStats] = strawberry.field(default_factory=list)
 
 
 @strawberry.type
 class EntityListsConnection(BaseConnection):
     edges: list[EntityListEdge] = strawberry.field(default_factory=list)
+    field_stats: list[ColumnStats] = strawberry.field(default_factory=list)
