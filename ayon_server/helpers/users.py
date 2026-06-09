08dc4c99ae6182176f59a5d4164432b08e78ca9b
Martastain <martas@imm.cz>
2026-06-09 12:34:59 +0200

    fix: type hint

--- ayon_server/helpers/users.py
+++ ayon_server/helpers/users.py
@@ -3,7 +3,7 @@ from ayon_server.lib.redis import Redis
 
 
 @Redis.cached("global", "manager-names", ttl=3600)
-async def get_manager_names() -> set[str]:
+async def get_manager_names() -> list[str]:
     """
     Returns a set of user names for all users with manager access.
     """
@@ -15,4 +15,4 @@ async def get_manager_names() -> set[str]:
         OR data->>'isAdmin' = 'true'
     """
     res = await Postgres.fetch(query)
-    return {row["name"] for row in res}
+    return [row["name"] for row in res]
