d8ce45e4a128174bdd7ee5475f94a2a0e937171a
Martastain <martas@imm.cz>
2026-06-12 14:17:57 +0200

    fix: optimize thumbnail source resolving, add db indexes

--- ayon_server/helpers/thumbnails/thumbnail_info_resolvers.py
+++ ayon_server/helpers/thumbnails/thumbnail_info_resolvers.py
@@ -14,37 +14,36 @@ async def resolve_folder_thumbnail_info(
 ) -> ThumbnailInfo:
     query = f"""
         WITH reviewables AS (
-            SELECT DISTINCT ON (f.id)
-            p.folder_id AS folder_id,
-            f.id AS reviewable_id,
-            v.thumbnail_id AS version_thumbnail_id
-            FROM project_{project_name}.files f
-            JOIN project_{project_name}.activity_feed a
-            ON a.activity_id = f.activity_id
-            AND a.entity_type = 'version'
-            AND a.activity_type = 'reviewable'
-            AND a.reference_type = 'origin'
-            JOIN project_{project_name}.versions v
-            ON v.id = a.entity_id
+                SELECT DISTINCT ON (f.id)
+                p.folder_id AS folder_id,
+                f.id AS reviewable_id,
+                v.thumbnail_id AS version_thumbnail_id
+            FROM project_{project_name}.folders entity
             JOIN project_{project_name}.products p
-            ON p.id = v.product_id
-            ORDER BY f.id, a.created_at DESC
+                ON p.folder_id = entity.id
+            JOIN project_{project_name}.versions v
+                ON v.product_id = p.id
+            JOIN project_{project_name}.activity_feed a
+                ON a.entity_id = v.id
+                AND a.entity_type = 'version'
+                AND a.activity_type = 'reviewable'
+                AND a.reference_type = 'origin'
+            JOIN project_{project_name}.files f
+                ON f.activity_id = a.activity_id
+            WHERE entity.id = $1
+            ORDER BY f.id, a.created_at DESC LIMIT 1
         )
         SELECT
             r.reviewable_id AS reviewable_id,
             r.version_thumbnail_id AS version_thumbnail_id,
             entity.thumbnail_id AS thumbnail_id,
-            hierarchy.path as path
-
+            hierarchy.path AS path
         FROM project_{project_name}.folders entity
-
-        INNER JOIN project_{project_name}.hierarchy as hierarchy
-        ON entity.id = hierarchy.id
-
+        INNER JOIN project_{project_name}.hierarchy AS hierarchy
+            ON entity.id = hierarchy.id
         LEFT JOIN reviewables r
-        ON r.folder_id = entity.id
-
-        WHERE entity.id = $1
+            ON r.folder_id = entity.id
+        WHERE entity.id = $1;
     """
 
     res = await Postgres.fetchrow(query, entity_id)
@@ -64,35 +63,39 @@ async def resolve_task_thumbnail_info(
     project_name: str,
     entity_id: str,
 ) -> ThumbnailInfo:
+
     query = f"""
         WITH reviewables AS (
             SELECT DISTINCT ON (v.id)
-            v.task_id AS task_id,
-            v.thumbnail_id AS version_thumbnail_id,
-            f.id AS reviewable_id
-            FROM project_{project_name}.files f
-            JOIN project_{project_name}.activity_feed a
-            ON a.activity_id = f.activity_id
-            AND a.entity_type = 'version'
-            AND a.activity_type = 'reviewable'
-            AND a.reference_type = 'origin'
+                v.task_id AS task_id,
+                v.thumbnail_id AS version_thumbnail_id,
+                f.id AS reviewable_id
+            FROM project_{project_name}.tasks entity
             JOIN project_{project_name}.versions v
-            ON v.id = a.entity_id
+                ON v.task_id = entity.id
+            JOIN project_{project_name}.activity_feed a
+                ON a.entity_id = v.id
+                AND a.entity_type = 'version'
+                AND a.activity_type = 'reviewable'
+                AND a.reference_type = 'origin'
+            JOIN project_{project_name}.files f
+                ON f.activity_id = a.activity_id
+            WHERE entity.id = $1
             ORDER BY v.id, a.created_at DESC
         )
         SELECT
             entity.thumbnail_id AS thumbnail_id,
             r.reviewable_id AS reviewable_id,
             r.version_thumbnail_id AS version_thumbnail_id,
-            hierarchy.path as folder_path
+            hierarchy.path AS folder_path
         FROM project_{project_name}.tasks entity
 
-        JOIN project_{project_name}.hierarchy as hierarchy
+        JOIN project_{project_name}.hierarchy AS hierarchy
             ON entity.folder_id = hierarchy.id
 
         LEFT JOIN reviewables r
-        ON r.task_id = entity.id
-        WHERE entity.id = $1
+            ON r.task_id = entity.id
+        WHERE entity.id = $1;
     """
 
     res = await Postgres.fetchrow(query, entity_id)
