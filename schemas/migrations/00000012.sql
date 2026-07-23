bb7b2c428312cf6714e69ecbd9d128c964046548
Martin Wacker <martas@imm.cz>
2026-07-23 15:50:06 +0200

    feat: more effective db migration

--- schemas/migrations/00000012.sql
+++ schemas/migrations/00000012.sql
@@ -2,30 +2,33 @@
 -- Add thumbnail_id column to project files
 -- 
 
-
 DO $$
 DECLARE rec RECORD;
 BEGIN
-  FOR rec IN SELECT t.table_schema, t.table_name
-    FROM information_schema.tables t
-    LEFT JOIN information_schema.columns c
-    ON t.table_schema = c.table_schema
-    AND t.table_name = c.table_name
-    AND c.column_name = 'thumbnail_id'
-    WHERE t.table_schema LIKE 'project_%'
-    AND t.table_name = 'files'
-    GROUP BY t.table_schema, t.table_name
-    HAVING COUNT(c.column_name) = 0
+  FOR rec IN 
+    SELECT ns.nspname AS project_schema, cl.relname AS table_name
+    FROM pg_namespace ns
+    JOIN pg_class cl 
+      ON cl.relnamespace = ns.oid
+    LEFT JOIN pg_attribute att 
+      ON att.attrelid = cl.oid 
+      AND att.attname = 'thumbnail_id'
+    WHERE 
+      ns.nspname LIKE 'project_%'
+      AND cl.relname = 'files'
+      AND att.attname IS NULL
     LOOP
         BEGIN
-          RAISE WARNING 'Adding thumbnail_id to %.%', rec.table_schema, rec.table_name;
-          EXECUTE 'SET LOCAL search_path TO ' || quote_ident(rec.table_schema);
+          RAISE WARNING 'Adding thumbnail_id to %.%', rec.project_schema, rec.table_name;
+          EXECUTE 'SET LOCAL search_path TO ' || quote_ident(rec.project_schema);
+
           ALTER TABLE files ADD COLUMN IF NOT EXISTS thumbnail_id UUID 
             REFERENCES thumbnails(id) ON DELETE SET NULL;
           CREATE INDEX IF NOT EXISTS idx_files_thumbnail_id ON files(thumbnail_id);
+
         EXCEPTION
           WHEN OTHERS THEN
-             RAISE WARNING 'Skipping schema % due to error: %', rec.table_schema, SQLERRM;
+             RAISE WARNING 'Skipping schema % due to error: %', rec.project_schema, SQLERRM;
         END;
     END LOOP;
     RETURN;
