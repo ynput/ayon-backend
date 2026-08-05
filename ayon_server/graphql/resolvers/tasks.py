089bf3e56dc010a672630de36e4beba07db1d421
petrk <petr.kalis@gmail.com>
2026-08-05 13:59:18 +0200

    Fix sorting by Folder Name in tasks resolver

--- ayon_server/graphql/resolvers/tasks.py
+++ ayon_server/graphql/resolvers/tasks.py
@@ -58,6 +58,7 @@ SORT_OPTIONS = {
     "updatedAt": "tasks.updated_at",
     "createdBy": "tasks.created_by",
     "updatedBy": "tasks.updated_by",
+    "folder": "folders.name",
 }
 
 
@@ -511,7 +512,7 @@ async def get_tasks(
     #
 
     # Do we need the parent folder data?
-    if use_folder_query or "folder" in fields:
+    if use_folder_query or "folder" in fields or sort_by == "folder":
         sql_columns.extend(
             [
                 "folders.id AS _folder_id",
