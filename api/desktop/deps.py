7d9289034823801d2aa99a596e9b0db12f42272f
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2026-08-07 00:48:28 +0100

    emit events on dependency package and installer uploads

--- api/desktop/deps.py
+++ api/desktop/deps.py
@@ -254,6 +254,15 @@ async def upload_dependency_package(
         raise AyonException("Filename in manifest does not match")
 
     await handle_upload(request, manifest.local_file_path)
+
+    await EventStream.dispatch(
+        "dependency_package.install",
+        description=f"Installed dependency_package: {filename}",
+        summary={"filename": filename},
+        user=user.name,
+        finished=True,
+    )
+
     await Redis.delete("desktop", "dependency-packages")
     return EmptyResponse(status_code=204)
 
