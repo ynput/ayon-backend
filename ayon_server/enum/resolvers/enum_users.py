efca9d6033290eb621ecdf6430100586e7a104be
Martin Wacker <martas@imm.cz>
2026-08-31 14:45:16 +0200

    feat: expand users enum resolver

--- ayon_server/enum/resolvers/enum_users.py
+++ ayon_server/enum/resolvers/enum_users.py
@@ -1,15 +1,17 @@
 from typing import Any
 
 from ayon_server.config import ayonconfig
+from ayon_server.entities import ProjectEntity
 from ayon_server.enum.base_resolver import BaseEnumResolver
 from ayon_server.enum.enum_item import EnumItem
 from ayon_server.exceptions import ForbiddenException
+from ayon_server.helpers.auth_utils import AuthUtils
 from ayon_server.lib.postgres import Postgres
 from ayon_server.models import IconModel
 from ayon_server.types import AttributeType
 
 query = """
-    SELECT name, attrib, data FROM public.users
+    SELECT name, attrib, data, active FROM public.users
     ORDER BY COALESCE(attrib->>'fullName', name)
 """
 
@@ -18,7 +20,13 @@ class UsersEnumResolver(BaseEnumResolver):
     name = "users"
 
     async def get_accepted_params(self) -> dict[str, AttributeType]:
-        return {"project_name": "string"}
+        return {
+            "project_name": "string",
+            "include_teams": "boolean",
+            "include_support": "boolean",
+            "hide_inactive": "boolean",  # Hide inactive users and users without license
+            "hide_users": "boolean",  # Hide all users (when listing teams only)
+        }
 
     async def resolve(self, context: dict[str, Any]) -> list[EnumItem]:
         result: list[EnumItem] = []
@@ -60,17 +68,13 @@ class UsersEnumResolver(BaseEnumResolver):
             if not current_user_ags:
                 raise ForbiddenException("You don't have access to this project")
 
-        def should_show_user(udata: dict[str, Any]) -> bool:
+        def should_list_user(udata: dict[str, Any]) -> bool:
+            """
+            Exclude users that the current user has no access to.
+            Excluded users won't be returned at all opposed to being returned as hidden
+            """
             is_admin = udata.get("isAdmin", False)
             is_manager = udata.get("isManager", False)
-
-            if (
-                current_user
-                and (not current_user.data.get("isSupport", False))
-                and udata.get("isSupport", False)
-            ):
-                # we don't show support users to non-support users
-                return False
 
             if is_admin or is_manager:
                 # we always show admins and managers
@@ -90,20 +94,69 @@ class UsersEnumResolver(BaseEnumResolver):
 
             return bool(set(ags).intersection(set(skip_if_not_ag)))
 
+        user_pool_ids = [
+            pool.id for pool in await AuthUtils.get_user_pools() if pool.valid
+        ]
+
+        def should_hide_user(
+            udata: dict[str, Any],
+            *,
+            active: bool = True,
+        ) -> bool:
+            """
+            Hide users that the current user has acccess to,
+            but should be hidden from the list. That allows displaying
+            them, but not selecting them.
+            """
+
+            if (
+                current_user
+                and (not current_user.data.get("isSupport", False))
+                and udata.get("isSupport", False)
+            ):
+                # we don't show support users to non-support users
+                return True
+
+            if context.get("hide_inactive"):
+                if not active:
+                    return True
+
+                if user_pool_ids and udata.get("userPool") not in user_pool_ids:
+                    return True
+
+            return False
+
         async with Postgres.transaction():
             stmt = await Postgres.prepare(query)
             async for row in stmt.cursor():
-                name, attrib, udata = row
+                name, attrib, udata, active = row
 
-                if not should_show_user(udata):
+                if not should_list_user(udata):
                     continue
 
                 item = EnumItem(
                     value=name,
                     label=attrib.get("fullName") or name,
+                    group="Users",
+                    hidden=should_hide_user(udata, active=active),
                     icon=IconModel(
                         type="url",
                         url=f"/api/users/{name}/avatar",
+                    ),
+                )
+                result.append(item)
+
+        if context.get("include_teams") and context.get("project_name"):
+            project = await ProjectEntity.load(context["project_name"])
+            for team in project.data.get("teams", []):
+                team_name = team.get("name") or "Unnamed Team"
+                item = EnumItem(
+                    value=f"team:{team_name}",
+                    label=team_name,
+                    group="Teams",
+                    icon=IconModel(
+                        type="material-symbols",
+                        name="group",
                     ),
                 )
                 result.append(item)
