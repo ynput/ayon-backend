10c95810c7a6c2c5900daab7eafe92eaa90f886d
Martin Wacker <martas@imm.cz>
2026-09-01 14:34:29 +0200

    refactor: remove nested functions

--- ayon_server/enum/resolvers/enum_users.py
+++ ayon_server/enum/resolvers/enum_users.py
@@ -1,7 +1,8 @@
+from collections.abc import Iterable
 from typing import Any
 
 from ayon_server.config import ayonconfig
-from ayon_server.entities import ProjectEntity
+from ayon_server.entities import ProjectEntity, UserEntity
 from ayon_server.enum.base_resolver import BaseEnumResolver
 from ayon_server.enum.enum_item import EnumItem
 from ayon_server.exceptions import ForbiddenException
@@ -14,6 +15,73 @@ query = """
     SELECT name, attrib, data, active FROM public.users
     ORDER BY COALESCE(attrib->>'fullName', name)
 """
+
+
+def should_list_user(
+    current_user: UserEntity | None,
+    context: dict[str, Any],
+    udata: dict[str, Any],
+    *,
+    project_name: str | None,
+    skip_if_not_ag: Iterable[str] | None,
+    has_all_user_access: bool,
+) -> bool:
+    """
+    Exclude users that the current user has no access to.
+    Excluded users won't be returned at all opposed to being returned as hidden
+    """
+    is_admin = udata.get("isAdmin", False)
+    is_manager = udata.get("isManager", False)
+
+    if is_admin or is_manager:
+        # we always show admins and managers
+        return True
+
+    if not project_name:
+        return has_all_user_access
+
+    # now we are in project scope
+
+    ags = udata.get("accessGroups", {}).get(project_name, [])
+    if not ags:
+        return False
+
+    if not skip_if_not_ag:
+        return True
+
+    return bool(set(ags).intersection(set(skip_if_not_ag)))
+
+
+def should_hide_user(
+    current_user: UserEntity | None,
+    context: dict[str, Any],
+    udata: dict[str, Any],
+    *,
+    active: bool = True,
+    user_pool_ids: list[str] | None = None,
+) -> bool:
+    """
+    Hide users that the current user has acccess to,
+    but should be hidden from the list. That allows displaying
+    them, but not selecting them.
+    """
+
+    if (
+        current_user
+        and (not current_user.data.get("isSupport", False))
+        and udata.get("isSupport", False)
+    ):
+        # we don't show support users to non-support users
+        return True
+
+    if context.get("hide_inactive"):
+        if not active:
+            return True
+
+        if user_pool_ids and (udata.get("userPool") not in user_pool_ids):
+            return True
+
+    return False
 
 
 class UsersEnumResolver(BaseEnumResolver):
@@ -67,77 +135,36 @@ class UsersEnumResolver(BaseEnumResolver):
             if not current_user_ags:
                 raise ForbiddenException("You don't have access to this project")
 
-        def should_list_user(udata: dict[str, Any]) -> bool:
-            """
-            Exclude users that the current user has no access to.
-            Excluded users won't be returned at all opposed to being returned as hidden
-            """
-            is_admin = udata.get("isAdmin", False)
-            is_manager = udata.get("isManager", False)
-
-            if is_admin or is_manager:
-                # we always show admins and managers
-                return True
-
-            if not project_name:
-                return has_all_user_access
-
-            # now we are in project scope
-
-            ags = udata.get("accessGroups", {}).get(project_name, [])
-            if not ags:
-                return False
-
-            if not skip_if_not_ag:
-                return True
-
-            return bool(set(ags).intersection(set(skip_if_not_ag)))
-
         user_pool_ids = [
             pool.id for pool in await AuthUtils.get_user_pools() if pool.valid
         ]
-
-        def should_hide_user(
-            udata: dict[str, Any],
-            *,
-            active: bool = True,
-        ) -> bool:
-            """
-            Hide users that the current user has acccess to,
-            but should be hidden from the list. That allows displaying
-            them, but not selecting them.
-            """
-
-            if (
-                current_user
-                and (not current_user.data.get("isSupport", False))
-                and udata.get("isSupport", False)
-            ):
-                # we don't show support users to non-support users
-                return True
-
-            if context.get("hide_inactive"):
-                if not active:
-                    return True
-
-                if user_pool_ids and (udata.get("userPool") not in user_pool_ids):
-                    return True
-
-            return False
 
         async with Postgres.transaction():
             stmt = await Postgres.prepare(query)
             async for row in stmt.cursor():
                 name, attrib, udata, active = row
 
-                if not should_list_user(udata):
+                if not should_list_user(
+                    current_user,
+                    context,
+                    udata,
+                    project_name=project_name,
+                    skip_if_not_ag=skip_if_not_ag,
+                    has_all_user_access=has_all_user_access,
+                ):
                     continue
 
                 item = EnumItem(
                     value=name,
                     label=attrib.get("fullName") or name,
                     group="Users",
-                    hidden=should_hide_user(udata, active=active),
+                    hidden=should_hide_user(
+                        current_user,
+                        context,
+                        udata,
+                        active=active,
+                        user_pool_ids=user_pool_ids,
+                    ),
                     icon=IconModel(
                         type="url",
                         url=f"/api/users/{name}/avatar",
